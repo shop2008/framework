16c3f6e9b6e6ae24ab9035605fbb25660fb2a1aa
neillin <>
2013-05-25 06:58:51 +0000

    init code

---
+++ wxxr-mobile-core/src/main/java/com/wxxr/mobile/core/api/UsernamePasswordCredential.java
@@ -0,0 +1,43 @@
+/**
+ * 
+ */
+package com.wxxr.mobile.core.api;
+
+/**
+ * @author neillin
+ *
+ */
+public class UsernamePasswordCredential implements IUserAuthCredential {
+
+	private String userName,password;
+	/* (non-Javadoc)
+	 * @see com.wxxr.mobile.core.api.IUserAuthCredential#getUserName()
+	 */
+	@Override
+	public String getUserName() {
+		return this.userName;
+	}
+
+	/* (non-Javadoc)
+	 * @see com.wxxr.mobile.core.api.IUserAuthCredential#getAuthPassword()
+	 */
+	@Override
+	public String getAuthPassword() {
+		return this.password;
+	}
+
+	/**
+	 * @param userName the userName to set
+	 */
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	/**
+	 * @param password the password to set
+	 */
+	public void setAuthPassword(String password) {
+		this.password = password;
+	}
+
+}
