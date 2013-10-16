06eb2df7260bae492801f3560c33df4d3a09ae73
neillin <>
2013-10-16 11:20:22 +0000

    refactoring

--- wxxr-mobile-android/src/main/java/com/wxxr/mobile/android/ui/MainActivity.java
+++ wxxr-mobile-android/src/main/java/com/wxxr/mobile/android/ui/MainActivity.java
@@ -15,6 +15,7 @@ import com.wxxr.mobile.core.ui.api.IWorkbenchManager;
  */
 public abstract class MainActivity extends Activity {
 
+	private int minStartupTime = 5;
 	/* (non-Javadoc)
 	 * @see android.app.Activity#onStart()
 	 */
@@ -22,14 +23,27 @@ public abstract class MainActivity extends Activity {
 	protected void onStart() {
 		super.onStart();
 		setupContentView();
+		startupKernel();
 	}
 
 	protected void startupKernel() {
 		final IProgressMonitor monitor = new IProgressMonitor() {
-			
+			private long minIntervalPerWork = 0L, lastUpdateTime,startTime;
+			private int totalWork,finishedWork;
 			@Override
 			public void updateProgress(int work, String message) {
+				int workDone = work - finishedWork;
+				finishedWork = work;
+				long interval = System.currentTimeMillis()-lastUpdateTime;
+				long minInterval = minIntervalPerWork*workDone;
+				if(interval < minInterval){
+					try {
+						Thread.sleep(minInterval - interval);
+					} catch (InterruptedException e) {
+					}
+				}
 				MainActivity.this.updateProgress(work, message);
+				this.lastUpdateTime = System.currentTimeMillis();
 			}
 			
 			@Override
@@ -49,11 +63,20 @@ public abstract class MainActivity extends Activity {
 			
 			@Override
 			public void done(Object result) {
+				updateProgressDone();
+				try {
+					Thread.sleep(1000L);
+				} catch (InterruptedException e) {
+				}
 				showHomePage();
 			}
 			
 			@Override
 			public void beginTask(int totalWork) {
+				this.totalWork = totalWork;
+				this.minIntervalPerWork = (minStartupTime*1000L)/totalWork;
+				this.startTime = System.currentTimeMillis();
+				this.lastUpdateTime = this.startTime;
 				showProgressBar(totalWork);
 			}
 		};
@@ -68,10 +91,25 @@ public abstract class MainActivity extends Activity {
 	protected abstract void setupContentView();
 	protected abstract void showProgressBar(int totalwork);
 	protected abstract void updateProgress(int workDone, String message);
+	protected abstract void updateProgressDone();
 	protected abstract void startupFailed(Throwable t, String message);
 	
 	protected void showHomePage(){
 		AppUtils.getService(IWorkbenchManager.class).getWorkbench().showHomePage();
 		this.finish();
+	}
+
+	/**
+	 * @return the minStartupTime
+	 */
+	protected int getMinStartupTime() {
+		return minStartupTime;
+	}
+
+	/**
+	 * @param minStartupTime the minStartupTime to set
+	 */
+	protected void setMinStartupTime(int minStartupTime) {
+		this.minStartupTime = minStartupTime;
 	}
 }
