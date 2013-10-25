7f2b0d530f3e22d86e8bba350b7e73b98f07ddbe
renwenjie <>
2013-10-25 08:44:24 +0000

    K线绘制用到的实体类 FECC-2484

--- wxxr-trading-app/wxxr-stock-android/src/main/java/com/wxxr/mobile/stock/client/biz/Kline.java
+++ wxxr-trading-app/wxxr-stock-android/src/main/java/com/wxxr/mobile/stock/client/biz/Kline.java
@@ -1,7 +1,13 @@
 package com.wxxr.mobile.stock.client.biz;
 
+/**
+ * K线绘制用到的实体类
+ * @author renwenjie
+ *
+ */
 public class Kline {
 
+	
 	public String date;
 	public String close;
 	public String open;
--- wxxr-trading-app/wxxr-stock-android/src/main/java/com/wxxr/mobile/stock/client/biz/Stock.java
+++ wxxr-trading-app/wxxr-stock-android/src/main/java/com/wxxr/mobile/stock/client/biz/Stock.java
@@ -1,19 +1,12 @@
-/*
- * @(#)Stock.java	 2011-12-19
- *
- * Copyright 2004-2011 WXXR Network Technology Co. Ltd. 
- * All rights reserved.
- * 
- * WXXR PROPRIETARY/CONFIDENTIAL.
- */
-
 package com.wxxr.mobile.stock.client.biz;
 
 /**
  * @author renwenjie
+ * 封装股票信息的实体类
  */
 public class Stock  {
 
+	
 	public String code;//股票代码
 	public String name;//股票名称
 
