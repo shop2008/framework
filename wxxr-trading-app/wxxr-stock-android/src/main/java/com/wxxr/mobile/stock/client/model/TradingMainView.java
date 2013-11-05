d2807c6a5e084025a9307f8e596423b1a51707bd
xijiadeng <>
2013-11-05 11:15:11 +0000

    *** empty log message ***

--- wxxr-trading-app/wxxr-stock-android/src/main/java/com/wxxr/mobile/stock/client/model/TradingMainView.java
+++ wxxr-trading-app/wxxr-stock-android/src/main/java/com/wxxr/mobile/stock/client/model/TradingMainView.java
@@ -3,15 +3,15 @@
  */
 package com.wxxr.mobile.stock.client.model;
 
-import java.util.ArrayList;
 import java.util.List;
-
 import com.wxxr.mobile.android.ui.AndroidBindingType;
 import com.wxxr.mobile.android.ui.annotation.AndroidBinding;
+import com.wxxr.mobile.core.ui.annotation.Command;
 import com.wxxr.mobile.core.ui.annotation.Field;
+import com.wxxr.mobile.core.ui.annotation.OnShow;
 import com.wxxr.mobile.core.ui.annotation.View;
-import com.wxxr.mobile.core.ui.api.IBinding;
-import com.wxxr.mobile.core.ui.api.IView;
+import com.wxxr.mobile.core.ui.api.InputEvent;
+import com.wxxr.mobile.core.ui.common.DataField;
 import com.wxxr.mobile.core.ui.common.ViewBase;
 import com.wxxr.mobile.stock.client.bean.Article;
 import com.wxxr.mobile.stock.client.module.IArticleManagerModule;
@@ -21,19 +21,28 @@ import com.wxxr.mobile.stock.client.module.IArticleManagerModule;
  *
  */
 @View(name="tradingMain")
-@AndroidBinding(type=AndroidBindingType.FRAGMENT,layoutId="R.layout.layout_home")
+@AndroidBinding(type=AndroidBindingType.FRAGMENT,layoutId="R.layout.home_view_layout")
 public abstract class TradingMainView extends ViewBase {
 	
-	//@Field(valueKey="options")
-	//List<Article> articles;
-
+	@Field(valueKey="options")
+	List<Article> articles;
+	DataField<List> articlesField;
 	
-	/* (non-Javadoc)
-	 * @see com.wxxr.mobile.core.ui.common.ViewBase#onShow(com.wxxr.mobile.core.ui.api.IBinding)
-	 */
-	@Override
-	protected void onShow(IBinding<IView> binding) {
-		//articles = getUIContext().getKernelContext().getService(IArticleManagerModule.class).getNewArticles(0, 4, 15);
+	@OnShow
+	protected void updataArticles() {
+		articles = getUIContext().getKernelContext().getService(IArticleManagerModule.class).getNewArticles(0, 4, 15);
+		articlesField.setValue(articles);
 	}
-	
+
+	/**
+	 * 创建买入页跳转
+	 * 
+	 * */
+	@Command(description="",commandName="createBuyClick")
+	String createBuyClick(InputEvent event){
+		if(InputEvent.EVENT_TYPE_CLICK.equals(event.getEventType())){
+//			getUIContext().getWorkbenchManager().getPageNavigator().showPage(arg0, null, null);
+		}
+		return null;
+	}
 }
