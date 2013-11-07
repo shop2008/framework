242efa6f30b3a9bf7e33a7119415422206d8ec45
wangxuyang <>
2013-11-07 06:55:35 +0000

    FECC-2498 帮助中心逻辑视图

--- wxxr-trading-app/wxxr-stock-android/src/main/java/com/wxxr/mobile/stock/client/model/HelpCenterItemView.java
+++ wxxr-trading-app/wxxr-stock-android/src/main/java/com/wxxr/mobile/stock/client/model/HelpCenterItemView.java
@@ -2,9 +2,11 @@ package com.wxxr.mobile.stock.client.model;
 
 import com.wxxr.mobile.android.ui.AndroidBindingType;
 import com.wxxr.mobile.android.ui.annotation.AndroidBinding;
+import com.wxxr.mobile.core.ui.annotation.Command;
 import com.wxxr.mobile.core.ui.annotation.Field;
 import com.wxxr.mobile.core.ui.annotation.View;
 import com.wxxr.mobile.core.ui.api.IModelUpdater;
+import com.wxxr.mobile.core.ui.api.InputEvent;
 import com.wxxr.mobile.core.ui.common.DataField;
 import com.wxxr.mobile.core.ui.common.ViewBase;
 import com.wxxr.mobile.stock.client.bean.Article;
@@ -23,6 +25,12 @@ public abstract class HelpCenterItemView extends ViewBase implements
 	DataField<String> titleField;
 	DataField<String> abstractInfoField;
 
+	
+	@Command(commandName="handleItemClick")
+	String handleItemClick(InputEvent event){
+		return null;
+	}
+	
 	@Override
 	public void updateModel(Object data) {
 		if (data instanceof Article) {
