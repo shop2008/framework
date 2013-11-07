616c16c3e6e49d4de182bdffcac3f43ba3cae72a
wangxuyang <>
2013-11-07 07:33:46 +0000

    FECC-2498 赚钱榜逻辑视图

---
+++ wxxr-trading-app/wxxr-stock-android/src/main/java/com/wxxr/mobile/stock/client/model/TradingWinnerItemView.java
@@ -0,0 +1,44 @@
+/**
+ * 
+ */
+package com.wxxr.mobile.stock.client.model;
+
+import com.wxxr.mobile.android.ui.AndroidBindingType;
+import com.wxxr.mobile.android.ui.annotation.AndroidBinding;
+import com.wxxr.mobile.core.ui.annotation.Command;
+import com.wxxr.mobile.core.ui.annotation.Field;
+import com.wxxr.mobile.core.ui.annotation.View;
+import com.wxxr.mobile.core.ui.api.IModelUpdater;
+import com.wxxr.mobile.core.ui.common.DataField;
+import com.wxxr.mobile.core.ui.common.ViewBase;
+
+/**
+ * @author wangxuyang
+ *
+ */
+@View(name="earnRankListItemView")
+@AndroidBinding(type=AndroidBindingType.VIEW,layoutId="R.layout.layout_elv_earn_money_item")
+public abstract class TradingWinnerItemView extends ViewBase implements IModelUpdater{
+	@Field(valueKey="text")
+	String text;
+	DataField<String> textField;
+	@Field(valueKey="imageURI")
+	String imageUrl;
+	DataField<String> imageUrlField;
+	String linkUrl;
+	
+	@Override
+	public void updateModel(Object value) {
+		
+		
+	}
+	
+	@Command(description="点击文本事件")
+	String handleTextClick(){
+		return null;
+	}
+	@Command(description="点击图片事件")
+	String handleImageClick(){
+		return null;
+	}
+}
--- wxxr-trading-app/wxxr-stock-android/src/main/java/com/wxxr/mobile/stock/client/model/TradingWinnerView.java
+++ wxxr-trading-app/wxxr-stock-android/src/main/java/com/wxxr/mobile/stock/client/model/TradingWinnerView.java
@@ -3,17 +3,33 @@
  */
 package com.wxxr.mobile.stock.client.model;
 
+import java.util.List;
+
 import com.wxxr.mobile.android.ui.AndroidBindingType;
 import com.wxxr.mobile.android.ui.annotation.AndroidBinding;
+import com.wxxr.mobile.core.ui.annotation.Field;
+import com.wxxr.mobile.core.ui.annotation.OnShow;
 import com.wxxr.mobile.core.ui.annotation.View;
+import com.wxxr.mobile.core.ui.common.DataField;
 import com.wxxr.mobile.core.ui.common.ViewBase;
+import com.wxxr.mobile.stock.client.bean.EarnRankItem;
+import com.wxxr.mobile.stock.client.service.IArticleManagementService;
 
 /**
+ * 赚钱榜逻辑视图
  * @author neillin
  *
  */
 @View(name="tradingWinner")
 @AndroidBinding(type=AndroidBindingType.FRAGMENT,layoutId="R.layout.layout_earn_money_rank")
 public abstract class TradingWinnerView extends ViewBase {
-
+	@Field(valueKey="options")
+	List<EarnRankItem> earnRankList;
+	DataField<List> earnRankListField;
+	
+	@OnShow	
+	protected void updateEarnRankList() {
+		earnRankList = null;//TODO 
+		earnRankListField.setValue(earnRankList);
+	}
 }
