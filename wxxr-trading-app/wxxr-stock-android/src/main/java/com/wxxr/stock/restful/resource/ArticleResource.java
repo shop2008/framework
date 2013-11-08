04784452e8c8e4cd6ebf3a355db32d9f2faca5b8
wangxuyang <>
2013-11-08 12:37:36 +0000

    FECC-2498 rest resource 迁移

---
+++ wxxr-trading-app/wxxr-stock-android/src/main/java/com/wxxr/stock/restful/resource/ArticleResource.java
@@ -0,0 +1,49 @@
+package com.wxxr.stock.restful.resource;
+
+import java.util.List;
+
+import com.wxxr.javax.ws.rs.GET;
+import com.wxxr.javax.ws.rs.POST;
+import com.wxxr.javax.ws.rs.Path;
+import com.wxxr.javax.ws.rs.Produces;
+import com.wxxr.javax.ws.rs.QueryParam;
+import com.wxxr.mobile.core.rpc.impl.ArticleVO;
+import com.wxxr.stock.restful.json.NewsDateQueryBO;
+import com.wxxr.stock.restful.json.NewsQueryBO;
+import com.wxxr.stock.restful.json.StockArticleQuery;
+import com.wxxr.stock.restful.json.UserArticleQueryBO;
+@Path("/rest/article")
+public interface ArticleResource  {
+
+	@POST
+    @Path("/getnewarticle")
+    @Produces( { "application/json" })
+    //@GZIP
+    public List<ArticleVO> getNewArticle(NewsQueryBO query) throws Exception ;
+	@POST
+    @Path("/getarticlebystock")
+    @Produces( { "application/json" })
+    //@GZIP
+    public List<ArticleVO> getArticleByStock(StockArticleQuery query) throws Exception ;
+		
+	
+	@POST
+    @Path("/getarticlebyuser")
+    @Produces( { "application/json" })
+    //@GZIP
+    public List<ArticleVO> getArticleByUser(UserArticleQueryBO query) throws Exception;
+	@POST
+    @Path("/getnew2cd")
+    @Produces( { "application/json" })
+    //@GZIP
+    public List<ArticleVO> getNew2Cd(NewsDateQueryBO query) throws Exception;
+	
+	@GET
+    @Path("/getarticlebyId")
+    @Produces({ "application/json", "application/xml" })
+    //@GZIP
+    public ArticleVO getNewArticleById(@QueryParam("articleId") String articleId,@QueryParam("type") String type) throws Exception;
+	
+	
+	
+}
