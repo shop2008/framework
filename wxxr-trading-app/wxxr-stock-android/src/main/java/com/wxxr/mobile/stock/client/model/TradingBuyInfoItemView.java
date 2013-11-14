44043b6e5ee3109e614f40906f3a26fd0cdcfdb1
xijiadeng <>
2013-11-14 05:39:15 +0000

    *** empty log message ***

--- wxxr-trading-app/wxxr-stock-android/src/main/java/com/wxxr/mobile/stock/client/model/TradingBuyInfoItemView.java
+++ wxxr-trading-app/wxxr-stock-android/src/main/java/com/wxxr/mobile/stock/client/model/TradingBuyInfoItemView.java
@@ -4,88 +4,83 @@ package com.wxxr.mobile.stock.client.model;
 import com.wxxr.mobile.android.ui.AndroidBindingType;
 import com.wxxr.mobile.android.ui.annotation.AndroidBinding;
 import com.wxxr.mobile.core.log.api.Trace;
+import com.wxxr.mobile.core.ui.annotation.Attribute;
 import com.wxxr.mobile.core.ui.annotation.Command;
 import com.wxxr.mobile.core.ui.annotation.Field;
 import com.wxxr.mobile.core.ui.annotation.View;
 import com.wxxr.mobile.core.ui.api.IModelUpdater;
 import com.wxxr.mobile.core.ui.api.InputEvent;
-import com.wxxr.mobile.core.ui.common.AttributeKeys;
-import com.wxxr.mobile.core.ui.common.DataField;
 import com.wxxr.mobile.core.ui.common.ViewBase;
 import com.wxxr.mobile.stock.client.bean.TradingAccountBean;
-import com.wxxr.mobile.stock.client.utils.ColorUtils;
 
 @View(name="TradingBuyInfoItemView")
 @AndroidBinding(type=AndroidBindingType.VIEW,layoutId="R.layout.trading_buy_info_item")
 public abstract class TradingBuyInfoItemView extends ViewBase implements IModelUpdater {
 	private static final Trace log = Trace.register(TradingMainView.class);
+	
+	TradingAccountBean trading;
+	int red = 0xffff0000;
+	int green;
 	//股票名称
-	@Field(valueKey="text")
+	@Field(valueKey="text",binding="${trading!=null?trading.stockName:'--'}",attributes={@Attribute(name = "textColor", value = "resourceId:color/yellow")})
 	String stockName;
-	DataField<String> stockNameField;
 	
 	//股票代码
-	@Field(valueKey="text")
+	@Field(valueKey="text",binding="${trading!=null?trading.stockCode:'--'}")
 	String stockCode;
-	DataField<String> stockCodeField;
 	
 	//额度（申请资金）
-	@Field(valueKey="text")
+	@Field(valueKey="text",binding="${trading!=null?trading.initCredit:'--'}")
 	String initCredit;
-	DataField<String> initCreditField;
 	
 	//总收益
-	@Field(valueKey="text")
+	@Field(valueKey="text",binding="${trading!=null?trading.income:'--'}")
 	String income;
-	DataField<String> incomeField;
 	
-	@Field(valueKey="enabled")
+	@Field(valueKey="enabled",binding="${trading!=null && trading.type==0?false:true}")
 	boolean type;//交易盘类型  0-模拟盘；1-实盘
-	DataField<Boolean> typeField;
 	
 	@Command
 	String testButtonClick(InputEvent event){
-//		Context context = AppUtils.getFramework().getAndroidApplication();
-//		Toast.makeText(context, "你点击了Button按钮", Toast.LENGTH_SHORT).show();
-		log.info("testButtonClick+++++++++++++++++++");
 		return null;
 	}
+	
 	
 	@Override
 	public void updateModel(Object data) {
 		if(data instanceof TradingAccountBean){
-			TradingAccountBean trading = (TradingAccountBean) data;
-			this.stockName = trading.getStockName();
-			this.stockNameField.setValue(this.stockName);
-			this.stockCode = trading.getStockCode();
-			this.stockCodeField.setValue(this.stockCode);
-			this.initCredit = String.valueOf(trading.getInitCredit());
-			this.initCreditField.setValue(this.initCredit);
-			float income = trading.getIncome();
-			if(income>0){
-				incomeField.setAttribute(AttributeKeys.textColor, ColorUtils.STOCK_RED);
-			}else if(income<0){
-				incomeField.setAttribute(AttributeKeys.textColor, ColorUtils.STOCK_GREEN);
-			}else{
-				incomeField.setAttribute(AttributeKeys.textColor, ColorUtils.STOCK_WHITE);
-			}
+//			trading = (TradingAccountBean) data;
+			registerBean("trading", data);
 			
-			if(trading.getStatus()==1){
-				stockNameField.setAttribute(AttributeKeys.textColor, ColorUtils.STOCK_GRAY);
-				stockCodeField.setAttribute(AttributeKeys.textColor, ColorUtils.STOCK_GRAY);
-				initCreditField.setAttribute(AttributeKeys.textColor, ColorUtils.STOCK_GRAY);
-				incomeField.setAttribute(AttributeKeys.textColor, ColorUtils.STOCK_GRAY);
-			}
-			this.income = String.valueOf(trading.getIncome());
-			this.incomeField.setValue(this.income);
-			if(trading.getType()==0){
-				type = false;
-			}else if(trading.getType()==1){
-				type = true;
-			}
-			this.typeField.setValue(this.type);
-//			this.status = trading.getStatus();
-//			incomeField.setAttribute(key, val)
+//			this.stockName = trading.getStockName();
+//			this.stockNameField.setValue(this.stockName);
+//			this.stockCode = trading.getStockCode();
+//			this.stockCodeField.setValue(this.stockCode);
+//			this.initCredit = String.valueOf(trading.getInitCredit());
+//			this.initCreditField.setValue(this.initCredit);
+//			float income = trading.getIncome();
+//			if(income>0){
+//				incomeField.setAttribute(AttributeKeys.textColor, ColorUtils.STOCK_RED);
+//			}else if(income<0){
+//				incomeField.setAttribute(AttributeKeys.textColor, ColorUtils.STOCK_GREEN);
+//			}else{
+//				incomeField.setAttribute(AttributeKeys.textColor, ColorUtils.STOCK_WHITE);
+//			}
+//			
+//			if(trading.getStatus()==1){
+//				DataField<String> stockNameField.setAttribute(AttributeKeys.textColor, ColorUtils.STOCK_GRAY);
+//				stockCodeField.setAttribute(AttributeKeys.textColor, ColorUtils.STOCK_GRAY);
+//				initCreditField.setAttribute(AttributeKeys.textColor, ColorUtils.STOCK_GRAY);
+//				incomeField.setAttribute(AttributeKeys.textColor, ColorUtils.STOCK_GRAY);
+//			}
+//			this.income = String.valueOf(trading.getIncome());
+//			this.incomeField.setValue(this.income);
+//			if(trading.getType()==0){
+//				type = false;
+//			}else if(trading.getType()==1){
+//				type = true;
+//			}
+//			this.typeField.setValue(this.type);
 		}
 	}
 }
--- wxxr-trading-app/wxxr-stock-android/src/main/java/com/wxxr/mobile/stock/client/model/TradingMainView.java
+++ wxxr-trading-app/wxxr-stock-android/src/main/java/com/wxxr/mobile/stock/client/model/TradingMainView.java
@@ -8,15 +8,18 @@ import java.util.List;
 import com.wxxr.mobile.android.ui.AndroidBindingType;
 import com.wxxr.mobile.android.ui.annotation.AndroidBinding;
 import com.wxxr.mobile.core.log.api.Trace;
+import com.wxxr.mobile.core.ui.annotation.Bean;
+import com.wxxr.mobile.core.ui.annotation.Bean.BindingType;
 import com.wxxr.mobile.core.ui.annotation.Command;
 import com.wxxr.mobile.core.ui.annotation.Field;
+import com.wxxr.mobile.core.ui.annotation.Navigation;
 import com.wxxr.mobile.core.ui.annotation.OnShow;
+import com.wxxr.mobile.core.ui.annotation.Parameter;
 import com.wxxr.mobile.core.ui.annotation.View;
 import com.wxxr.mobile.core.ui.api.InputEvent;
-import com.wxxr.mobile.core.ui.common.DataField;
 import com.wxxr.mobile.core.ui.common.ViewBase;
-import com.wxxr.mobile.stock.client.StockAppBizException;
 import com.wxxr.mobile.stock.client.bean.ArticleBean;
+import com.wxxr.mobile.stock.client.bean.MyArticlesBean;
 import com.wxxr.mobile.stock.client.bean.TradingAccountBean;
 import com.wxxr.mobile.stock.client.service.IArticleManagementService;
 import com.wxxr.mobile.stock.client.service.ITradingManagementService;
@@ -29,59 +32,46 @@ import com.wxxr.mobile.stock.client.service.ITradingManagementService;
 @AndroidBinding(type=AndroidBindingType.FRAGMENT,layoutId="R.layout.home_view_layout")
 public abstract class TradingMainView extends ViewBase{
 	private static final Trace log = Trace.register(TradingMainView.class);
-	//文章
-	@Field(valueKey="options")
+	
+	
+	/**获取文章*/
+	@Bean(type=BindingType.Service)
+	IArticleManagementService articleService;
+	
+	@Bean(type=BindingType.Pojo,express="${articleService.getMyArticles(0, 4, 15)}")
+	MyArticlesBean myArticles;
+	
+	/**绑定文章*/
+	@Field(valueKey="options",binding="${myArticles!=null?myArticles.homeArticles:null}")
 	List<ArticleBean> articles;
-	DataField<List> articlesField;
 	
 	
+	/**获取T日数据*/
+	@Bean(type=BindingType.Service)
+	ITradingManagementService tradingService;
 	
-	//T日
-	@Field(valueKey="options")
+	@Bean(type=BindingType.Pojo,express="${tradingService.getTradingAccountList(0)}")
+	List<TradingAccountBean> tradingAccount;
+
+	@Field(valueKey="options",binding="${tradingAccount!=null?tradingAccount:null}")
 	List<TradingAccountBean> tradingT;
-	DataField<List> tradingTField;
 	
-	@Field(valueKey="enabled")
-	boolean isVisibleT; //是否显示
-	DataField<Boolean> isVisibleTField;
 	
-	//T+1日
-	@Field(valueKey="options")
+	/**获取T+1日数据*/
+	
+	@Bean(type=BindingType.Pojo,express="${tradingService.getTradingAccountList(1)}")
+	List<TradingAccountBean> tradingAccount1;
+
+	@Field(valueKey="options",binding="${tradingAccount1!=null?tradingAccount1:null}")
 	List<TradingAccountBean> tradingT1;
-	DataField<List> tradingT1Field;
 	
-	@Field(valueKey="enabled")
-	boolean isVisibleT1; //是否显示
-	DataField<Boolean> isVisibleT1Field;
+	
+	
+	/**交易盘类型  0-模拟盘；1-实盘*/
+	String type;
 	
 	@OnShow
 	protected void updataArticles() {
-		//获取文章数据
-		articles = getUIContext().getKernelContext().getService(IArticleManagementService.class).getNewArticles(0, 4, 15);
-		articlesField.setValue(articles);
-		
-		try {
-			//获取T日数据
-			List<TradingAccountBean> tempTrading = getUIContext().getKernelContext().getService(ITradingManagementService.class).getTradingAccountList(0);
-			if(tempTrading!=null && tempTrading.size()>0){
-				tradingT = tempTrading;
-				tradingTField.setValue(tradingT);
-				isVisibleT = true;
-			}else{
-				isVisibleT = false;
-			}
-			//获取T+1日数据
-			List<TradingAccountBean> tempTrading1 = getUIContext().getKernelContext().getService(ITradingManagementService.class).getTradingAccountList(1);
-			if(tempTrading1!=null && tempTrading1.size()>0){
-				tradingT1 = tempTrading1;
-				tradingT1Field.setValue(tradingT1);
-				isVisibleT1 = true;
-			}else{
-				isVisibleT1 = false;
-			}
-		} catch (StockAppBizException e) {
-			
-		}
 	}
 
 	/**
@@ -91,10 +81,41 @@ public abstract class TradingMainView extends ViewBase{
 	@Command
 	String createBuyClick(InputEvent event){
 		if(InputEvent.EVENT_TYPE_CLICK.equals(event.getEventType())){
-			if(getUIContext()!=null){
-				log.info("createBuyClick: click me");
-			}
-//			getUIContext().getWorkbenchManager().getPageNavigator().showPage(arg0, null, null);
+			getUIContext().getWorkbenchManager().getPageNavigator().showPage(getUIContext().getWorkbenchManager().getWorkbench().getPage("creataBuyTradePage"), null, null);
+		}
+		return null;
+	}
+	
+	//点击T日列表跳转
+	@Command
+	String T_TradingMessageClick(InputEvent event){
+		return null;
+	}
+	//点击T+1日列表跳转
+	
+	
+//	@Command
+//	String T1_TradingMessageClick(InputEvent event){
+//		if(InputEvent.EVENT_TYPE_ITEM_CLICK.equals(event.getEventType())){
+//			getUIContext().getWorkbenchManager().getPageNavigator().showPage(getUIContext().getWorkbenchManager().getWorkbench().getPage("OperationDetails"), null, null);
+//		}
+//		return null;
+//	}
+	
+	@Command(
+			navigations={
+				@Navigation(on="operationDetails",showPage="OperationDetails",params={@Parameter(name="stockType",value="${type}")})
+				})
+	String tradingMessageClick(InputEvent event){
+		if(InputEvent.EVENT_TYPE_ITEM_CLICK.equals(event.getEventType())){
+//			if(event.getProperty("position") instanceof Integer){
+//				int position = (Integer) event.getProperty("position");
+//				if(tradingT1!=null && tradingT1.size()>0){
+//					TradingAccount tempTradingA = tradingT1.get(position);
+//					this.type = String.valueOf(tempTradingA.getType());
+//				}
+//			}
+			return "operationDetails";
 		}
 		return null;
 	}
