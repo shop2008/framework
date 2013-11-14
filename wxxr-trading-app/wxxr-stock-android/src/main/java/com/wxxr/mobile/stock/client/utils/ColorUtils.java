19b1e908126a45dd36d04d42743bed927bbf9753
xijiadeng <>
2013-11-14 10:25:03 +0000

    FECC-2505

--- wxxr-trading-app/wxxr-stock-android/src/main/java/com/wxxr/mobile/stock/client/utils/ColorUtils.java
+++ wxxr-trading-app/wxxr-stock-android/src/main/java/com/wxxr/mobile/stock/client/utils/ColorUtils.java
@@ -3,12 +3,12 @@ package com.wxxr.mobile.stock.client.utils;
 import android.graphics.Color;
 
 public class ColorUtils {
-	public static final int STOCK_RED = Color.parseColor("#BA2514"); //红色
-	public static final int STOCK_GREEN = Color.parseColor("#3C7F00"); // 绿色
-	public static final int STOCK_YELLOW = Color.parseColor("#FFE400");//黄色
-	public static final int STOCK_WHITE = Color.parseColor("#FFFFFF"); // 白色
-	public static final int STOCK_BLACK = Color.parseColor("#000000"); //黑色
-	public static final int STOCK_GRAY = Color.parseColor("#ACACAC"); //灰色
-	public static final int STOCK_BLUE = Color.parseColor("#0966be"); //蓝色
+	public static final String STOCK_RED = "#BA2514"; //红色
+	public static final String STOCK_GREEN = "#3C7F00"; // 绿色
+	public static final String STOCK_YELLOW = "#FFE400";//黄色
+	public static final String STOCK_WHITE = "#FFFFFF"; // 白色
+	public static final String STOCK_BLACK = "#000000"; //黑色
+	public static final String STOCK_GRAY = "#ACACAC"; //灰色
+	public static final String STOCK_BLUE = "#0966be"; //蓝色
 	
 }
