ef8339b53e8d4326a7fe93be62c999eda2e1f75a
duzhen <>
2013-11-14 12:16:19 +0000

    FECC-2528

--- wxxr-trading-app/wxxr-stock-android/src/main/java/com/wxxr/mobile/stock/client/model/ChampionShipItemView.java
+++ wxxr-trading-app/wxxr-stock-android/src/main/java/com/wxxr/mobile/stock/client/model/ChampionShipItemView.java
@@ -2,6 +2,7 @@ package com.wxxr.mobile.stock.client.model;
 
 import com.wxxr.mobile.android.ui.AndroidBindingType;
 import com.wxxr.mobile.android.ui.annotation.AndroidBinding;
+import com.wxxr.mobile.core.ui.annotation.Bean;
 import com.wxxr.mobile.core.ui.annotation.Field;
 import com.wxxr.mobile.core.ui.annotation.View;
 import com.wxxr.mobile.core.ui.api.IModelUpdater;
@@ -13,7 +14,10 @@ import com.wxxr.mobile.stock.client.bean.MegagameRankBean;
 @AndroidBinding(type = AndroidBindingType.VIEW, layoutId = "R.layout.champion_ship_page_layout_item")
 public abstract class ChampionShipItemView extends ViewBase implements
 		IModelUpdater {
-
+	
+	@Bean
+	MegagameRankBean msgRank;
+	
 	@Field(valueKey = "text")
 	String maxStockCode;
 	DataField<String> maxStockCodeField;
@@ -37,21 +41,23 @@ public abstract class ChampionShipItemView extends ViewBase implements
 	@Override
 	public void updateModel(Object value) {
 		if (value instanceof MegagameRankBean) {
-			MegagameRankBean msgRank = (MegagameRankBean) value;
-			this.rankSeq = String.valueOf(msgRank.getRankSeq());
-			this.rankSeqField.setValue(this.rankSeq);
-
-			this.nickName = msgRank.getNickName();
-			this.nickNameField.setValue(this.nickName);
-
-			this.maxStockCode = msgRank.getMaxStockCode();
-			this.maxStockCodeField.setValue(this.maxStockCode);
-
-			this.maxStockMarket = msgRank.getMaxStockMarket();
-			this.maxStockMarketField.setValue(this.maxStockMarket);
-
-			this.gainRate = msgRank.getGainRate();
-			this.gainRateField.setValue(this.gainRate);
+//			MegagameRankBean msgRank = (MegagameRankBean) value;
+			registerBean("msgRank",value);
+			
+//			this.rankSeq = String.valueOf(msgRank.getRankSeq());
+//			this.rankSeqField.setValue(this.rankSeq);
+//
+//			this.nickName = msgRank.getNickName();
+//			this.nickNameField.setValue(this.nickName);
+//
+//			this.maxStockCode = msgRank.getMaxStockCode();
+//			this.maxStockCodeField.setValue(this.maxStockCode);
+//
+//			this.maxStockMarket = msgRank.getMaxStockMarket();
+//			this.maxStockMarketField.setValue(this.maxStockMarket);
+//
+//			this.gainRate = msgRank.getGainRate();
+//			this.gainRateField.setValue(this.gainRate);
 		}
 	}
 }
--- wxxr-trading-app/wxxr-stock-android/src/main/java/com/wxxr/mobile/stock/client/model/ChampionShipView.java
+++ wxxr-trading-app/wxxr-stock-android/src/main/java/com/wxxr/mobile/stock/client/model/ChampionShipView.java
@@ -3,22 +3,24 @@
  */
 package com.wxxr.mobile.stock.client.model;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import com.wxxr.mobile.android.ui.AndroidBindingType;
 import com.wxxr.mobile.android.ui.annotation.AndroidBinding;
 import com.wxxr.mobile.core.log.api.Trace;
+import com.wxxr.mobile.core.ui.annotation.Bean;
+import com.wxxr.mobile.core.ui.annotation.Bean.BindingType;
 import com.wxxr.mobile.core.ui.annotation.Command;
 import com.wxxr.mobile.core.ui.annotation.Field;
+import com.wxxr.mobile.core.ui.annotation.OnCreate;
 import com.wxxr.mobile.core.ui.annotation.OnShow;
 import com.wxxr.mobile.core.ui.annotation.View;
 import com.wxxr.mobile.core.ui.api.InputEvent;
-import com.wxxr.mobile.core.ui.common.AttributeKeys;
 import com.wxxr.mobile.core.ui.common.DataField;
 import com.wxxr.mobile.core.ui.common.ViewBase;
 import com.wxxr.mobile.stock.client.StockAppBizException;
 import com.wxxr.mobile.stock.client.bean.MegagameRankBean;
+import com.wxxr.mobile.stock.client.bean.RankListBean;
 import com.wxxr.mobile.stock.client.bean.RegularTicketBean;
 import com.wxxr.mobile.stock.client.bean.WeekRankBean;
 import com.wxxr.mobile.stock.client.binding.IRefreshCallback;
@@ -33,43 +35,53 @@ import com.wxxr.mobile.stock.client.service.ITradingManagementService;
 public abstract class ChampionShipView extends ViewBase {
 	private static final Trace log = Trace.register(TradingMainView.class);
 
-	@Field(valueKey = "options")
+	@Bean(type=BindingType.Service)
+	ITradingManagementService tradingMgr;
+	
+	@Bean(type=BindingType.Pojo,express="${tradingMgr.getTMegagameRank()}")
+	RankListBean ChampionShipBean;
+	
+	@Field(valueKey = "options", binding="${ChampionShipBean!=null?ChampionShipBean.tRankBeans:null}",visibleWhen="${currentViewId == 1}")
 	List<MegagameRankBean> ChampionShip;
-	DataField<List> ChampionShipField;
-
-	@Field(valueKey = "options")
+	
+	@Field(valueKey = "options", binding="${ChampionShipBean!=null?ChampionShipBean.t1RankBeans:null}",visibleWhen="${currentViewId == 2}")
 	List<MegagameRankBean> ChampionT1Ship;
-	DataField<List> ChampionT1ShipField;
 
-	@Field(valueKey = "options")
+	@Field(valueKey = "options", binding="${ChampionShipBean!=null?ChampionShipBean.weekRanKBeans:null}",visibleWhen="${currentViewId == 3}")
 	List<WeekRankBean> ChampionWeekShip;
-	DataField<List> ChampionWeekShipField;
 
-	@Field(valueKey = "options")
+	@Field(valueKey = "options", binding="${ChampionShipBean!=null?ChampionShipBean.regularTicketBeans:null}",visibleWhen="${currentViewId == 4}")
 	List<RegularTicketBean> ChampionRegularShip;
+	
+//	@Field
+//	int currentViewId;
+//	
+	DataField<List> ChampionShipField;
+	DataField<List> ChampionT1ShipField;
+	DataField<List> ChampionWeekShipField;
 	DataField<List> ChampionRegularShipField;
-
 	@OnShow
 	protected void updataMegagameRank() {
-		try {
-			ChampionShip = getUIContext().getKernelContext()
-					.getService(ITradingManagementService.class)
-					.getTMegagameRank();
-			ChampionT1Ship = new ArrayList<MegagameRankBean>();
-			ChampionWeekShip = new ArrayList<WeekRankBean>();
-			ChampionRegularShip = new ArrayList<RegularTicketBean>();
-		} catch (StockAppBizException e) {
-
-		}
-		ChampionShipField.setValue(this.ChampionShip);
-		ChampionT1ShipField.setValue(this.ChampionT1Ship);
-		ChampionWeekShipField.setValue(this.ChampionWeekShip);
-		ChampionRegularShipField.setValue(this.ChampionRegularShip);
-		
-		ChampionShipField.setAttribute(AttributeKeys.visible, true);
-		ChampionT1ShipField.setAttribute(AttributeKeys.visible, false);
-		ChampionWeekShipField.setAttribute(AttributeKeys.visible, false);
-		ChampionRegularShipField.setAttribute(AttributeKeys.visible, false);
+		registerBean("currentViewId", 1);
+//		try {
+//			ChampionShip = getUIContext().getKernelContext()
+//					.getService(ITradingManagementService.class)
+//					.getTMegagameRank();
+//			ChampionT1Ship = new ArrayList<MegagameRankBean>();
+//			ChampionWeekShip = new ArrayList<WeekRankBean>();
+//			ChampionRegularShip = new ArrayList<RegularTicketBean>();
+//		} catch (StockAppBizException e) {
+//
+//		}
+//		ChampionShipField.setValue(this.ChampionShip);
+//		ChampionT1ShipField.setValue(this.ChampionT1Ship);
+//		ChampionWeekShipField.setValue(this.ChampionWeekShip);
+//		ChampionRegularShipField.setValue(this.ChampionRegularShip);
+//		
+//		ChampionShipField.setAttribute(AttributeKeys.visible, true);
+//		ChampionT1ShipField.setAttribute(AttributeKeys.visible, false);
+//		ChampionWeekShipField.setAttribute(AttributeKeys.visible, false);
+//		ChampionRegularShipField.setAttribute(AttributeKeys.visible, false);
 	}
 
 	@Command
@@ -79,13 +91,15 @@ public abstract class ChampionShipView extends ViewBase {
 		}
 		IRefreshCallback cb = (IRefreshCallback)event.getProperty("callback");
 		ChampionShip.clear();
-		handleTMegaClick(null);
+		try {
+			getUIContext().getKernelContext()
+					.getService(ITradingManagementService.class)
+					.getTMegagameRank();
+		} catch (StockAppBizException e) {
+
+		}
 		if(cb!=null)
 			cb.refreshSuccess();
-//		IStockAppToolbar tool = ((IStockAppToolbar)((PageBase)getUIContext().getWorkbenchManager().getWorkbench().getPage("home")).getap;
-//		if(tool != null){
-//			tool.showNotification("当前时间 :"+fmt.format(new Date()), null);
-//		}
 		return null;
 		
 	}
@@ -97,7 +111,13 @@ public abstract class ChampionShipView extends ViewBase {
 		}
 		IRefreshCallback cb = (IRefreshCallback)event.getProperty("callback");
 		ChampionShip.clear();
-		handleTMega1Click(null);
+		try {
+			getUIContext().getKernelContext()
+					.getService(ITradingManagementService.class)
+					.getT1MegagameRank();
+		} catch (StockAppBizException e) {
+
+		}
 		if(cb!=null)
 			cb.refreshSuccess();
 		return null;
@@ -111,7 +131,13 @@ public abstract class ChampionShipView extends ViewBase {
 		}
 		IRefreshCallback cb = (IRefreshCallback)event.getProperty("callback");
 		ChampionShip.clear();
-		handleWeekClick(null);
+		try {
+			getUIContext().getKernelContext()
+					.getService(ITradingManagementService.class)
+					.getWeekRank();
+		} catch (StockAppBizException e) {
+
+		}
 		if(cb!=null)
 			cb.refreshSuccess();
 		return null;
@@ -125,7 +151,13 @@ public abstract class ChampionShipView extends ViewBase {
 		}
 		IRefreshCallback cb = (IRefreshCallback)event.getProperty("callback");
 		ChampionShip.clear();
-		handleRegularTicketClick(null);
+		try {
+			getUIContext().getKernelContext()
+					.getService(ITradingManagementService.class)
+					.getRegularTicketRank();
+		} catch (StockAppBizException e) {
+
+		}
 		if(cb!=null)
 			cb.refreshSuccess();
 		return null;
@@ -134,84 +166,75 @@ public abstract class ChampionShipView extends ViewBase {
 	
 	@Command
 	String handleTMegaClick(InputEvent event) {
-		if(ChampionShip.size() == 0) {
-			try {
-				List<MegagameRankBean> ship = getUIContext().getKernelContext()
-						.getService(ITradingManagementService.class)
-						.getTMegagameRank();
-				ChampionShip.addAll(ship);
-			} catch (StockAppBizException e) {
-
-			}
-		}
-		ChampionShipField.setValue(this.ChampionShip);
-		ChampionShipField.setAttribute(AttributeKeys.visible, true);
-		ChampionT1ShipField.setAttribute(AttributeKeys.visible, false);
-		ChampionWeekShipField.setAttribute(AttributeKeys.visible, false);
-		ChampionRegularShipField.setAttribute(AttributeKeys.visible, false);
+		registerBean("currentViewId", 1);
 		return null;
 
 	}
 
 	@Command
 	String handleTMega1Click(InputEvent event) {
-		if(ChampionT1Ship.size() == 0) {
-			try {
-				List<MegagameRankBean> ship = getUIContext().getKernelContext()
-						.getService(ITradingManagementService.class)
-						.getT1MegagameRank();
-				ChampionT1Ship.addAll(ship);
-			} catch (StockAppBizException e) {
-
-			}
-		}
-		ChampionT1ShipField.setValue(this.ChampionT1Ship);
-		ChampionShipField.setAttribute(AttributeKeys.visible, false);
-		ChampionT1ShipField.setAttribute(AttributeKeys.visible, true);
-		ChampionWeekShipField.setAttribute(AttributeKeys.visible, false);
-		ChampionRegularShipField.setAttribute(AttributeKeys.visible, false);
+		registerBean("currentViewId", 2);
+//		if(ChampionT1Ship.size() == 0) {
+//			try {
+//				getUIContext().getKernelContext()
+//						.getService(ITradingManagementService.class).getT1MegagameRank();
+////						.getT1MegagameRank();
+////				ChampionT1Ship.addAll(ship);
+//			} catch (StockAppBizException e) {
+//
+//			}
+//		}
+//		ChampionT1ShipField.setValue(this.ChampionT1Ship);
+//		ChampionShipField.setAttribute(AttributeKeys.visible, false);
+//		ChampionT1ShipField.setAttribute(AttributeKeys.visible, true);
+//		ChampionWeekShipField.setAttribute(AttributeKeys.visible, false);
+//		ChampionRegularShipField.setAttribute(AttributeKeys.visible, false);
 		return null;
 
 	}
 
 	@Command
 	String handleWeekClick(InputEvent event) {
-		if(ChampionWeekShip.size() == 0) {
-			try {
-				List<WeekRankBean> ship = getUIContext().getKernelContext()
-						.getService(ITradingManagementService.class).getWeekRank();
-				ChampionWeekShip.addAll(ship);
-			} catch (StockAppBizException e) {
-
-			}
-		}
-		ChampionWeekShipField.setValue(this.ChampionWeekShip);
-		ChampionShipField.setAttribute(AttributeKeys.visible, false);
-		ChampionT1ShipField.setAttribute(AttributeKeys.visible, false);
-		ChampionWeekShipField.setAttribute(AttributeKeys.visible, true);
-		ChampionRegularShipField.setAttribute(AttributeKeys.visible, false);
+		registerBean("currentViewId", 3);
+//		if(ChampionWeekShip.size() == 0) {
+////			try {
+////				List<WeekRankBean> ship = getUIContext().getKernelContext()
+////						.getService(ITradingManagementService.class).getWeekRank();
+////				ChampionWeekShip.addAll(ship);
+////			} catch (StockAppBizException e) {
+////
+////			}
+//		}
+//		ChampionWeekShipField.setValue(this.ChampionWeekShip);
+////		ChampionShipField.setAttribute(AttributeKeys.visible, false);
+//		ChampionT1ShipField.setAttribute(AttributeKeys.visible, false);
+//		ChampionWeekShipField.setAttribute(AttributeKeys.visible, true);
+//		ChampionRegularShipField.setAttribute(AttributeKeys.visible, false);
 		return null;
 
 	}
 
 	@Command
 	String handleRegularTicketClick(InputEvent event) {
-		if(ChampionRegularShip.size() == 0)	 {
-			try {
-				List<RegularTicketBean> ship = getUIContext().getKernelContext()
-						.getService(ITradingManagementService.class)
-						.getRegularTicketRank();
-				ChampionRegularShip.addAll(ship);
-			} catch (StockAppBizException e) {
-
-			}
-		}
-		ChampionRegularShipField.setValue(this.ChampionRegularShip);
-		ChampionShipField.setAttribute(AttributeKeys.visible, false);
-		ChampionT1ShipField.setAttribute(AttributeKeys.visible, false);
-		ChampionWeekShipField.setAttribute(AttributeKeys.visible, false);
-		ChampionRegularShipField.setAttribute(AttributeKeys.visible, true);
+		registerBean("currentViewId", 4);
+//		if(ChampionRegularShip.size() == 0)	 {
+////			try {
+////				List<RegularTicketBean> ship = getUIContext().getKernelContext()
+////						.getService(ITradingManagementService.class)
+////						.getRegularTicketRank();
+////				ChampionRegularShip.addAll(ship);
+////			} catch (StockAppBizException e) {
+////
+////			}
+//		}
+//		ChampionRegularShipField.setValue(this.ChampionRegularShip);
+////		ChampionShipField.setAttribute(AttributeKeys.visible, false);
+//		ChampionT1ShipField.setAttribute(AttributeKeys.visible, false);
+//		ChampionWeekShipField.setAttribute(AttributeKeys.visible, false);
+//		ChampionRegularShipField.setAttribute(AttributeKeys.visible, true);
 		return null;
 
 	}
+	
+
 }
