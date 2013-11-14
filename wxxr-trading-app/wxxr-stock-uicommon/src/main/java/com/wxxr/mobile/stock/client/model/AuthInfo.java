d1f1ba573b193aeea9869689a748752e6aacc218
renwenjie <>
2013-11-14 11:50:27 +0000

    *** empty log message ***

---
+++ wxxr-trading-app/wxxr-stock-uicommon/src/main/java/com/wxxr/mobile/stock/client/model/AuthInfo.java
@@ -0,0 +1,20 @@
+package com.wxxr.mobile.stock.client.model;
+
+import com.wxxr.mobile.core.annotation.BindableBean;
+
+import android.R.string;
+@BindableBean(pkg="com.wxxr.mobile.stock.client.bean",className="AuthInfoBean")
+public class AuthInfo {
+
+	/**用户名*/
+	private String accountName;
+	
+	/**开户行*/
+	private String bankName;
+	
+	/**开户行所在地*/
+	private String bankAddr;
+	
+	/**银行卡号*/
+	private String bankNum;
+}
