f393d82451b68f58d6dd22b85999a8cc05f61c4d
neillin <>
2013-11-14 12:43:06 +0000

    refactoring

---
+++ wxxr-mobile-core/src/main/java/com/wxxr/mobile/core/bean/api/SetDecorator.java
@@ -0,0 +1,278 @@
+/**
+ * 
+ */
+package com.wxxr.mobile.core.bean.api;
+
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
+import java.util.Set;
+
+/**
+ * @author neillin
+ *
+ */
+public class SetDecorator<E> implements Set<E> {
+	
+	private List<E> data;
+	final private PropertyChangeSupport support;
+	final private String name;
+	
+	public SetDecorator(String propertyName,PropertyChangeSupport p){
+		this.support = p;
+		this.name = propertyName;
+	}
+
+	/**
+	 * @return the data
+	 */
+	public List<E> getData() {
+		return data;
+	}
+
+	/**
+	 * @param data the data to set
+	 */
+	public void setData(List<E> data) {
+		this.data = data;
+	}
+
+	/**
+	 * @param object
+	 * @return
+	 * @see java.util.List#add(java.lang.Object)
+	 */
+	public boolean add(E object) {
+		 data.add(object);
+		 this.support.fireIndexedPropertyChange(name, data.size()-1, null, object);
+		 return true;
+	}
+
+	/**
+	 * @param location
+	 * @param object
+	 * @see java.util.List#add(int, java.lang.Object)
+	 */
+	public void add(int location, E object) {
+		data.add(location, object);
+		this.support.fireIndexedPropertyChange(name, location, null, object);
+	}
+
+	/**
+	 * @param arg0
+	 * @return
+	 * @see java.util.List#addAll(java.util.Collection)
+	 */
+	public boolean addAll(Collection<? extends E> arg0) {
+		 data.addAll(arg0);
+		 this.support.firePropertyChange(name, null, this);
+		 return true;
+	}
+
+	/**
+	 * @param arg0
+	 * @param arg1
+	 * @return
+	 * @see java.util.List#addAll(int, java.util.Collection)
+	 */
+	public boolean addAll(int arg0, Collection<? extends E> arg1) {
+		 data.addAll(arg0, arg1);
+		 this.support.firePropertyChange(name, null, this);
+		 return true;
+	}
+
+	/**
+	 * 
+	 * @see java.util.List#clear()
+	 */
+	public void clear() {
+		data.clear();
+		 this.support.firePropertyChange(name, this, null);
+	}
+
+	/**
+	 * @param object
+	 * @return
+	 * @see java.util.List#contains(java.lang.Object)
+	 */
+	public boolean contains(Object object) {
+		return data.contains(object);
+	}
+
+	/**
+	 * @param arg0
+	 * @return
+	 * @see java.util.List#containsAll(java.util.Collection)
+	 */
+	public boolean containsAll(Collection<?> arg0) {
+		return data.containsAll(arg0);
+	}
+
+	/**
+	 * @param object
+	 * @return
+	 * @see java.util.List#equals(java.lang.Object)
+	 */
+	public boolean equals(Object object) {
+		return data.equals(object);
+	}
+
+	/**
+	 * @param location
+	 * @return
+	 * @see java.util.List#get(int)
+	 */
+	public E get(int location) {
+		return data.get(location);
+	}
+
+	/**
+	 * @return
+	 * @see java.util.List#hashCode()
+	 */
+	public int hashCode() {
+		return data.hashCode();
+	}
+
+	/**
+	 * @param object
+	 * @return
+	 * @see java.util.List#indexOf(java.lang.Object)
+	 */
+	public int indexOf(Object object) {
+		return data.indexOf(object);
+	}
+
+	/**
+	 * @return
+	 * @see java.util.List#isEmpty()
+	 */
+	public boolean isEmpty() {
+		return data.isEmpty();
+	}
+
+	/**
+	 * @return
+	 * @see java.util.List#iterator()
+	 */
+	public Iterator<E> iterator() {
+		return data.iterator();
+	}
+
+	/**
+	 * @param object
+	 * @return
+	 * @see java.util.List#lastIndexOf(java.lang.Object)
+	 */
+	public int lastIndexOf(Object object) {
+		return data.lastIndexOf(object);
+	}
+
+	/**
+	 * @return
+	 * @see java.util.List#listIterator()
+	 */
+	public ListIterator<E> listIterator() {
+		return data.listIterator();
+	}
+
+	/**
+	 * @param location
+	 * @return
+	 * @see java.util.List#listIterator(int)
+	 */
+	public ListIterator<E> listIterator(int location) {
+		return data.listIterator(location);
+	}
+
+	/**
+	 * @param location
+	 * @return
+	 * @see java.util.List#remove(int)
+	 */
+	public E remove(int location) {
+		E val = data.remove(location);
+		 this.support.firePropertyChange(name, this, null);
+		return val;
+	}
+
+	/**
+	 * @param object
+	 * @return
+	 * @see java.util.List#remove(java.lang.Object)
+	 */
+	public boolean remove(Object object) {
+		boolean val = data.remove(object);
+		 this.support.firePropertyChange(name, this, null);
+		return val;
+	}
+
+	/**
+	 * @param arg0
+	 * @return
+	 * @see java.util.List#removeAll(java.util.Collection)
+	 */
+	public boolean removeAll(Collection<?> arg0) {
+		boolean val = data.removeAll(arg0);
+		this.support.firePropertyChange(name, this, null);
+		return val;
+	}
+
+	/**
+	 * @param arg0
+	 * @return
+	 * @see java.util.List#retainAll(java.util.Collection)
+	 */
+	public boolean retainAll(Collection<?> arg0) {
+		return data.retainAll(arg0);
+	}
+
+	/**
+	 * @param location
+	 * @param object
+	 * @return
+	 * @see java.util.List#set(int, java.lang.Object)
+	 */
+	public E set(int location, E object) {
+		E val = data.set(location, object);
+		 this.support.fireIndexedPropertyChange(name, 0,val, object);
+		return val;
+	}
+
+	/**
+	 * @return
+	 * @see java.util.List#size()
+	 */
+	public int size() {
+		return data.size();
+	}
+
+	/**
+	 * @param start
+	 * @param end
+	 * @return
+	 * @see java.util.List#subList(int, int)
+	 */
+	public List<E> subList(int start, int end) {
+		return data.subList(start, end);
+	}
+
+	/**
+	 * @return
+	 * @see java.util.List#toArray()
+	 */
+	public Object[] toArray() {
+		return data.toArray();
+	}
+
+	/**
+	 * @param array
+	 * @return
+	 * @see java.util.List#toArray(T[])
+	 */
+	public <T> T[] toArray(T[] array) {
+		return data.toArray(array);
+	}
+
+}
