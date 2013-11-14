5ff3c757f7362685e9b19ba795d78017510c28d7
wangxuyang <>
2013-11-14 09:15:21 +0000

    FECC-2564 refactoring

--- wxxr-trading-app/wxxr-stock-uicommon/src/main/java/com/wxxr/mobile/stock/security/ILoginContext.java
+++
@@ -1,7 +0,0 @@
-package com.wxxr.mobile.stock.security;
-
-import javax.security.auth.login.LoginException;
-
-public interface ILoginContext {
-	public boolean login(String userName, String passwd) throws LoginException;
-}
--- wxxr-trading-app/wxxr-stock-uicommon/src/main/java/com/wxxr/mobile/stock/security/INamePasswdCallBack.java
+++
@@ -1,5 +0,0 @@
-package com.wxxr.mobile.stock.security;
-
-public interface INamePasswdCallBack {
-	public void callback(String userName,String passwd);
-}
