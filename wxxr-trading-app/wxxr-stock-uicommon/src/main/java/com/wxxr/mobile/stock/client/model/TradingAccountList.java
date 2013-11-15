73563df788b01bc91bde98e5043ac5af0531906e
wangxuyang <>
2013-11-15 02:49:58 +0000

    FECC-2564

--- wxxr-trading-app/wxxr-stock-uicommon/src/main/java/com/wxxr/mobile/stock/client/model/TradingAccountList.java
+++ wxxr-trading-app/wxxr-stock-uicommon/src/main/java/com/wxxr/mobile/stock/client/model/TradingAccountList.java
@@ -18,4 +18,6 @@ public class TradingAccountList {
 	private List<TradingAccountBean> allTradingAccounts;//所有
 	private List<TradingAccountBean> t0TradingAccounts;//T日
 	private List<TradingAccountBean> t1TradingAccountBeans;//T+1日
+	private List<TradingAccountBean> virtualTradingAccountBeans;//参赛交易盘
+	private List<TradingAccountBean> realTradingAccountBeans;//挑战交易盘
 }
