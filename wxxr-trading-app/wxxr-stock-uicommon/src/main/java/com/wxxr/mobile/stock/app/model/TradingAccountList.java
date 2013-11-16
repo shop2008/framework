b4266ce4b762c9526fc1d13929c4d78b18187912
wangxuyang <>
2013-11-16 06:41:09 +0000

    FECC-2564

--- wxxr-trading-app/wxxr-stock-uicommon/src/main/java/com/wxxr/mobile/stock/app/model/TradingAccountList.java
+++ wxxr-trading-app/wxxr-stock-uicommon/src/main/java/com/wxxr/mobile/stock/app/model/TradingAccountList.java
@@ -7,6 +7,7 @@ package com.wxxr.mobile.stock.app.model;
 import java.util.List;
 
 import com.wxxr.mobile.core.annotation.BindableBean;
+import com.wxxr.mobile.stock.app.bean.TradingAccInfoBean;
 import com.wxxr.mobile.stock.app.bean.TradingAccountBean;
 
 /**
@@ -15,9 +16,9 @@ import com.wxxr.mobile.stock.app.bean.TradingAccountBean;
  */
 @BindableBean(pkg="com.wxxr.mobile.stock.app.bean",className="TradingAccountListBean")
 public class TradingAccountList {
+	private List<TradingAccInfoBean> t0TradingAccounts;//T日
+	private List<TradingAccInfoBean> t1TradingAccountBeans;//T+1日
 	private List<TradingAccountBean> allTradingAccounts;//所有
-	private List<TradingAccountBean> t0TradingAccounts;//T日
-	private List<TradingAccountBean> t1TradingAccountBeans;//T+1日
 	private List<TradingAccountBean> virtualTradingAccountBeans;//参赛交易盘
 	private List<TradingAccountBean> realTradingAccountBeans;//挑战交易盘
 	private List<TradingAccountBean> successTradingAccountBeans; //成功交易记录
