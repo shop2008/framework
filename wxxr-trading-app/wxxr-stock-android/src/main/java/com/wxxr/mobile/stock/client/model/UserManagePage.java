8461b0858001b7e3ec039e6342abddb9a01cd718
renwenjie <>
2013-11-19 02:20:46 +0000

    FECC-2548

--- wxxr-trading-app/wxxr-stock-android/src/main/java/com/wxxr/mobile/stock/client/model/UserManagePage.java
+++ wxxr-trading-app/wxxr-stock-android/src/main/java/com/wxxr/mobile/stock/client/model/UserManagePage.java
@@ -2,19 +2,21 @@ package com.wxxr.mobile.stock.client.model;
 
 import com.wxxr.mobile.android.ui.AndroidBindingType;
 import com.wxxr.mobile.android.ui.annotation.AndroidBinding;
+import com.wxxr.mobile.core.ui.annotation.Bean;
 import com.wxxr.mobile.core.ui.annotation.Command;
 import com.wxxr.mobile.core.ui.annotation.Navigation;
 import com.wxxr.mobile.core.ui.annotation.View;
+import com.wxxr.mobile.core.ui.annotation.Bean.BindingType;
 import com.wxxr.mobile.core.ui.api.InputEvent;
 import com.wxxr.mobile.core.ui.common.PageBase;
+import com.wxxr.mobile.stock.app.service.IUserManagementService;
 
 @View(name="userManagePage")
 @AndroidBinding(type=AndroidBindingType.FRAGMENT_ACTIVITY, layoutId="R.layout.user_manage_layout")
 public abstract class UserManagePage extends PageBase {
 
-	
-	
-	
+	@Bean(type = BindingType.Service)
+	IUserManagementService usrService;
 	
 	@Command(
 			commandName = "modifyPsw", 
@@ -45,4 +47,9 @@ public abstract class UserManagePage extends PageBase {
 		return null;
 	}
 
+	@Command
+	String loginOut(InputEvent event) {
+		usrService.logout();
+		return null;
+	}
 }
