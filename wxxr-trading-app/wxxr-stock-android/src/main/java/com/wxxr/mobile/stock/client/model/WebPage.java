511af7748b2e04077c4280b55b1dc617c684128c
xijiadeng <>
2013-11-21 02:32:43 +0000

    *** empty log message ***

--- wxxr-trading-app/wxxr-stock-android/src/main/java/com/wxxr/mobile/stock/client/model/WebPage.java
+++ wxxr-trading-app/wxxr-stock-android/src/main/java/com/wxxr/mobile/stock/client/model/WebPage.java
@@ -4,39 +4,45 @@ import java.util.Map;
 
 import com.wxxr.mobile.android.ui.AndroidBindingType;
 import com.wxxr.mobile.android.ui.annotation.AndroidBinding;
+import com.wxxr.mobile.core.log.api.Trace;
+import com.wxxr.mobile.core.ui.annotation.Bean;
 import com.wxxr.mobile.core.ui.annotation.Field;
+import com.wxxr.mobile.core.ui.annotation.OnShow;
 import com.wxxr.mobile.core.ui.annotation.View;
-import com.wxxr.mobile.core.ui.api.IBinding;
 import com.wxxr.mobile.core.ui.api.IModelUpdater;
-import com.wxxr.mobile.core.ui.api.IView;
-import com.wxxr.mobile.core.ui.common.DataField;
 import com.wxxr.mobile.core.ui.common.PageBase;
 
 @View(name="webPage")
 @AndroidBinding(type=AndroidBindingType.ACTIVITY, layoutId="R.layout.web_page_layout")
 public abstract class WebPage extends PageBase implements IModelUpdater{
 
-	@Field(valueKey="webUrl")
+	static Trace log= Trace.getLogger(WebPage.class);
+	@Bean
+	String tempUrl = "#";
+	
+	@Field(valueKey="webUrl",binding="${tempUrl!=null?tempUrl:'#'}")
 	String webUrl;
-	DataField<String> webUrlField;
 	
 	
-	@Override
-	protected void onShow(IBinding<IView> binding) {
-		
+	
+	@OnShow
+	void initUrl(){
+		registerBean("tempUrl", tempUrl);
 	}
 	
 	@Override
 	public void updateModel(Object value) {
-		if(value instanceof Map){
-			Map temp = (Map) value;
-			for(Object key:temp.keySet()){
-				String url = (String) temp.get(key);
-				if(url!=null && "webUrl".equals(key)){
-					this.webUrl = url;
-					this.webUrlField.setValue(this.webUrl);
-				}
-			}
-		}
+	    if (value instanceof Map) {
+	        Map temp = (Map)value;
+	        for (Object key : temp.keySet()) {
+	            String tempt = (String)temp.get(key);
+	            if (tempt != null && "result".equals(key)) {
+	            	log.info("WebPage webUrl=" + tempt);
+	            	this.webUrl = tempt;
+	            	this.tempUrl = tempt;
+	            	registerBean("tempUrl", tempt);
+	            }
+	        }
+	    }
 	}
 }
