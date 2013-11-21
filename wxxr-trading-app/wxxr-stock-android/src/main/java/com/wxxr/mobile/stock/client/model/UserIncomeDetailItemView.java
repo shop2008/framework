ce5de879f749223ed763244ce00714d466c0ce32
renwenjie <>
2013-11-21 14:12:18 +0000

    *** empty log message ***

--- wxxr-trading-app/wxxr-stock-android/src/main/java/com/wxxr/mobile/stock/client/model/UserIncomeDetailItemView.java
+++ wxxr-trading-app/wxxr-stock-android/src/main/java/com/wxxr/mobile/stock/client/model/UserIncomeDetailItemView.java
@@ -4,14 +4,14 @@ package com.wxxr.mobile.stock.client.model;
 
 import com.wxxr.mobile.android.ui.AndroidBindingType;
 import com.wxxr.mobile.android.ui.annotation.AndroidBinding;
-import com.wxxr.mobile.core.ui.annotation.Bean;
+import com.wxxr.mobile.core.ui.annotation.Convertor;
 import com.wxxr.mobile.core.ui.annotation.Field;
-import com.wxxr.mobile.core.ui.annotation.OnShow;
+import com.wxxr.mobile.core.ui.annotation.Parameter;
 import com.wxxr.mobile.core.ui.annotation.View;
 import com.wxxr.mobile.core.ui.api.IModelUpdater;
 import com.wxxr.mobile.core.ui.common.ViewBase;
 import com.wxxr.mobile.stock.app.bean.TradeDetailBean;
-import com.wxxr.mobile.stock.client.utils.Utils;
+import com.wxxr.mobile.stock.client.utils.Float2StringConvertor;
 @View(name="incomeDetailsItemView")
 @AndroidBinding(type=AndroidBindingType.VIEW, layoutId="R.layout.income_details_item_layout")
 public abstract class UserIncomeDetailItemView extends ViewBase implements IModelUpdater {
@@ -22,19 +22,15 @@ public abstract class UserIncomeDetailItemView extends ViewBase implements IMode
 	@Field(valueKey = "text", binding="${detailBean!=null?detailBean.tradeDate:'---'}")
 	String incomeDate;
 	
-	@Field(valueKey = "text", binding="${detailBean!=null?util.parseFloat(detailBean.tradeAmount):'---'}")
+	@Field(valueKey = "text", binding="${detailBean!=null?detailBean.tradeAmount:'---'}", converter="float2StringConvertor")
 	String incomeAmount;
 
 	TradeDetailBean detailBean;
 	
-	@Bean
-	Utils util = Utils.getInstance();
-	
-	@OnShow
-	protected void initData() {
-		registerBean("util", util);
-	}
-	
+	@Convertor(
+			params={@Parameter(name="format", value="%+10.2f")}
+			)
+	Float2StringConvertor float2StringConvertor;
 	@Override
 	public void updateModel(Object value) {
 		if (value instanceof TradeDetailBean) {
