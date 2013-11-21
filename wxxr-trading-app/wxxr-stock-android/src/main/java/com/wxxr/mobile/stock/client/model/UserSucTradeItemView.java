af0f4bbc4a77888ca578825c6399915779f8bc99
renwenjie <>
2013-11-21 14:03:38 +0000

    FECC-2560

--- wxxr-trading-app/wxxr-stock-android/src/main/java/com/wxxr/mobile/stock/client/model/UserSucTradeItemView.java
+++ wxxr-trading-app/wxxr-stock-android/src/main/java/com/wxxr/mobile/stock/client/model/UserSucTradeItemView.java
@@ -3,11 +3,15 @@ package com.wxxr.mobile.stock.client.model;
 import com.wxxr.mobile.android.ui.AndroidBindingType;
 import com.wxxr.mobile.android.ui.annotation.AndroidBinding;
 import com.wxxr.mobile.core.ui.annotation.Attribute;
+import com.wxxr.mobile.core.ui.annotation.Convertor;
 import com.wxxr.mobile.core.ui.annotation.Field;
+import com.wxxr.mobile.core.ui.annotation.Parameter;
 import com.wxxr.mobile.core.ui.annotation.View;
 import com.wxxr.mobile.core.ui.api.IModelUpdater;
 import com.wxxr.mobile.core.ui.common.ViewBase;
 import com.wxxr.mobile.stock.app.bean.GainBean;
+import com.wxxr.mobile.stock.client.utils.LongTime2StringConvertor;
+import com.wxxr.mobile.stock.client.utils.StockLong2StringAutoUnitConvertor;
 
 @View(name="tradeRecordMoreItemView")
 @AndroidBinding(type=AndroidBindingType.VIEW, layoutId="R.layout.user_trade_record_item_layout")
@@ -36,16 +40,27 @@ public abstract class UserSucTradeItemView extends ViewBase implements IModelUpd
 	/**总收益*/
 	@Field(valueKey="text",binding="${accountBean!=null?accountBean.userGain:'--'}",attributes={
 			@Attribute(name = "textColor", value = "${accountBean.status == 1 ? 'resourceId:color/gray': (accountBean.userGain > 0 ? 'resourceId:color/red' : (accountBean.userGain < 0 ? 'resourceId:color/green':'resourceId:color/white'))}")
-			})	
+			},
+			converter="stockLong2StringAutoUnitConvertor"
+			)	
 	String income;
 	
-	@Field(valueKey="text", binding="${accountBean!=null?accountBean.closeTime:'--'}")
+	@Field(valueKey="text", binding="${accountBean!=null?accountBean.closeTime:'--'}", converter="longTime2StringConvertor")
 	String date;
 	
 	/**交易盘类型  0-模拟盘；1-实盘*/
 	@Field(valueKey="enabled", binding="${accountBean!=null && accountBean.virtual==true?true:false}")
 	boolean type;
 	
+	@Convertor(params={
+			@Parameter(name="format",value="MM-dd")
+	})
+	LongTime2StringConvertor longTime2StringConvertor;
+	
+	@Convertor(params={
+			@Parameter(name="format", value="%10.2f")
+	})
+	StockLong2StringAutoUnitConvertor stockLong2StringAutoUnitConvertor;
 	
 	@Override
 	public void updateModel(Object value) {
