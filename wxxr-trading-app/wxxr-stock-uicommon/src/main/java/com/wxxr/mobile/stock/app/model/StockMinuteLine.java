7ff610829d482d15853b18a3c03ff5b237c3e840
wangxuyang <>
2013-11-22 06:57:12 +0000

    MSTOCKB-186

--- wxxr-trading-app/wxxr-stock-uicommon/src/main/java/com/wxxr/mobile/stock/app/model/StockMinuteLine.java
+++ wxxr-trading-app/wxxr-stock-uicommon/src/main/java/com/wxxr/mobile/stock/app/model/StockMinuteLine.java
@@ -12,7 +12,7 @@ public class StockMinuteLine{
 	private Long price;// 价格(最新价)
 	private Long secuvolume;// 成交量
 	private Long secuamount;// 成交额
-	private Long avprice;// 成交额
+	private Long avprice;// 均线价格
 	private String hqTime;//补充时间，为了测试
 	private Long avgChangeRate;//上证指数，和深圳成指数的平均涨跌幅
 	
