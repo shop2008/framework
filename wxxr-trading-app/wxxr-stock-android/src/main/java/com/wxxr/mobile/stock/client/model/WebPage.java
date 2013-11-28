db8f6bcdb22a375dd9dc438d73bd6d7d6528bf45
xijiadeng <>
2013-11-28 09:12:11 +0000

    *** empty log message ***

--- wxxr-trading-app/wxxr-stock-android/src/main/java/com/wxxr/mobile/stock/client/model/WebPage.java
+++ wxxr-trading-app/wxxr-stock-android/src/main/java/com/wxxr/mobile/stock/client/model/WebPage.java
@@ -6,23 +6,35 @@ import com.wxxr.mobile.android.ui.AndroidBindingType;
 import com.wxxr.mobile.android.ui.annotation.AndroidBinding;
 import com.wxxr.mobile.core.log.api.Trace;
 import com.wxxr.mobile.core.ui.annotation.Bean;
+import com.wxxr.mobile.core.ui.annotation.Command;
 import com.wxxr.mobile.core.ui.annotation.Field;
+import com.wxxr.mobile.core.ui.annotation.Menu;
 import com.wxxr.mobile.core.ui.annotation.OnShow;
+import com.wxxr.mobile.core.ui.annotation.UIItem;
 import com.wxxr.mobile.core.ui.annotation.View;
+import com.wxxr.mobile.core.ui.api.IMenu;
 import com.wxxr.mobile.core.ui.api.IModelUpdater;
+import com.wxxr.mobile.core.ui.api.InputEvent;
 import com.wxxr.mobile.core.ui.common.PageBase;
 
-@View(name="webPage")
+@View(name="webPage",withToolbar=true,description="文章正文")
 @AndroidBinding(type=AndroidBindingType.ACTIVITY, layoutId="R.layout.web_page_layout")
 public abstract class WebPage extends PageBase implements IModelUpdater{
 
+	@Menu(items = { "left" })
+	private IMenu toolbar;
+
+	@Command(description = "Invoke when a toolbar item was clicked", uiItems = { @UIItem(id = "left", label = "返回", icon = "resourceId:drawable/back_button") })
+	String toolbarClickedLeft(InputEvent event) {
+		getUIContext().getWorkbenchManager().getPageNavigator().hidePage(this);
+		return null;
+	}
 	static Trace log= Trace.getLogger(WebPage.class);
 	@Bean
 	String tempUrl = "#";
 	
 	@Field(valueKey="webUrl",binding="${tempUrl!=null?tempUrl:'#'}")
 	String webUrl;
-	
 	
 	
 	@OnShow
