52135c2de36f27d83bb5aad4c05f27e3609b27f2
neillin <>
2013-12-01 10:26:43 +0000

    refactoring

--- wxxr-mobile-tools/src/main/java/com/wxxr/mobile/core/tools/TestProcessorConfigure.java
+++ wxxr-mobile-tools/src/main/java/com/wxxr/mobile/core/tools/TestProcessorConfigure.java
@@ -3,9 +3,7 @@
  */
 package com.wxxr.mobile.core.tools;
 
-import com.wxxr.mobile.core.annotation.BindableBean;
 import com.wxxr.mobile.core.tools.annotation.Generator;
-import com.wxxr.mobile.core.tools.generator.BindableBeanGenerator;
 import com.wxxr.mobile.core.tools.generator.DaoGenerator;
 import com.wxxr.mobile.core.tools.generator.UIViewModelGenerator;
 import com.wxxr.mobile.core.ui.annotation.View;
@@ -19,10 +17,10 @@ import com.wxxr.mobile.dao.annotation.Entity;
 //@SupportedOptions({"version=2.1.0"})
 public class TestProcessorConfigure implements IProcessorConfigure {
 
-	@Generator(forAnnotation=BindableBean.class)
-	public BindableBeanGenerator getBindableBeanGenerator(Generator annotation){
-		return new BindableBeanGenerator();
-	}
+//	@Generator(forAnnotation=BindableBean.class)
+//	public BindableBeanGenerator getBindableBeanGenerator(Generator annotation){
+//		return new BindableBeanGenerator();
+//	}
 	@Generator(forAnnotation=View.class)
 	public UIViewModelGenerator getUIPageModelGenerator(Generator annotation){
 		return new UIViewModelGenerator();
