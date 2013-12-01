e57f58a090f83a636a9b7402f0d2dff1d21dddfb
renwenjie <>
2013-12-01 09:36:09 +0000

    *** empty log message ***

---
+++ wxxr-trading-app/wxxr-stock-uicommon/src/main/java/com/wxxr/mobile/stock/app/model/UseSwitchCardCallBack.java
@@ -0,0 +1,34 @@
+package com.wxxr.mobile.stock.app.model;
+
+public class UseSwitchCardCallBack {
+	private String bankName;
+	
+	private String bankAddr;
+	
+	private String bankNum;
+
+	public String getBankName() {
+		return bankName;
+	}
+
+	public void setBankName(String bankName) {
+		this.bankName = bankName;
+	}
+
+	public String getBankAddr() {
+		return bankAddr;
+	}
+
+	public void setBankAddr(String bankAddr) {
+		this.bankAddr = bankAddr;
+	}
+
+	public String getBankNum() {
+		return bankNum;
+	}
+
+	public void setBankNum(String bankNum) {
+		this.bankNum = bankNum;
+	}
+	
+}
