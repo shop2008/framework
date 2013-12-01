08520d7acebe9e1b1898521201af4037e28bd384
renwenjie <>
2013-12-01 05:46:42 +0000

    *** empty log message ***

---
+++ wxxr-trading-app/wxxr-stock-uicommon/src/main/java/com/wxxr/mobile/stock/app/model/UserRegCallback.java
@@ -0,0 +1,14 @@
+package com.wxxr.mobile.stock.app.model;
+
+public class UserRegCallback {
+	
+	private String userName;
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+}
