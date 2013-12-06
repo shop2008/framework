f144b28e3cd2cd5635d666407222090f9f30da74
renwenjie <>
2013-12-06 09:11:17 +0000

    *** empty log message ***

---
+++ wxxr-trading-app/wxxr-stock-uicommon/src/main/java/com/wxxr/mobile/stock/app/model/UserFindPswCallBack.java
@@ -0,0 +1,14 @@
+package com.wxxr.mobile.stock.app.model;
+
+public class UserFindPswCallBack {
+
+	private String phoneNum;
+
+	public String getPhoneNum() {
+		return phoneNum;
+	}
+
+	public void setPhoneNum(String phoneNum) {
+		this.phoneNum = phoneNum;
+	}
+}
