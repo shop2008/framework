8dbfb6176465e550d74cf1dcef3c2ee5839e9945
duzhen <>
2013-12-12 03:10:36 +0000

    FECC-2510 FECC-2511 FECC-2512 FECC-2513

--- wxxr-trading-app/wxxr-stock-android/src/main/java/com/wxxr/mobile/stock/client/model/TBuyTradingItemOrderView.java
+++ wxxr-trading-app/wxxr-stock-android/src/main/java/com/wxxr/mobile/stock/client/model/TBuyTradingItemOrderView.java
@@ -7,8 +7,12 @@ import com.wxxr.mobile.core.ui.annotation.Bean.BindingType;
 import com.wxxr.mobile.core.ui.annotation.Command;
 import com.wxxr.mobile.core.ui.annotation.Convertor;
 import com.wxxr.mobile.core.ui.annotation.Field;
+import com.wxxr.mobile.core.ui.annotation.Menu;
+import com.wxxr.mobile.core.ui.annotation.Navigation;
 import com.wxxr.mobile.core.ui.annotation.Parameter;
+import com.wxxr.mobile.core.ui.annotation.UIItem;
 import com.wxxr.mobile.core.ui.annotation.View;
+import com.wxxr.mobile.core.ui.api.IMenu;
 import com.wxxr.mobile.core.ui.api.IModelUpdater;
 import com.wxxr.mobile.core.ui.api.InputEvent;
 import com.wxxr.mobile.core.ui.common.ViewBase;
@@ -25,26 +29,24 @@ public abstract class TBuyTradingItemOrderView extends ViewBase implements
 	// 交易服务
 	@Bean(type = BindingType.Service)
 	ITradingManagementService manageService;
-	
-	//查股票名称
+
+	// 查股票名称
 	@Bean(type = BindingType.Service)
 	IStockInfoSyncService stockInfoSyncService;
 
 	@Bean(type = BindingType.Pojo, express = "${stockInfoSyncService.getStockBaseInfoByCode(orderBean!=null?orderBean.stockCode:'', orderBean!=null?orderBean.marketCode:'')}")
 	StockBaseInfo stockInfoBean;
-	
+
 	@Bean
 	StockTradingOrderBean orderBean;
-	
-	@Convertor(params={
-			@Parameter(name="format",value="%.2f元"),
-			@Parameter(name="multiple", value="100.00")
-	})
+
+	@Convertor(params = { @Parameter(name = "format", value = "%.2f元"),
+			@Parameter(name = "multiple", value = "100.00") })
 	StockLong2StringConvertor stockLong2StringConvertorNoSign;
-	
+
 	@Field(valueKey = "text", binding = "${stockInfoBean!=null?stockInfoBean.name:'--'}")
 	String stockName;
-	
+
 	@Field(valueKey = "text", binding = "${orderBean!=null?orderBean.buy:'0'}", converter = "stockLong2StringConvertorNoSign")
 	String buy;
 
@@ -56,22 +58,41 @@ public abstract class TBuyTradingItemOrderView extends ViewBase implements
 
 	@Field(valueKey = "text", binding = "${orderBean!=null?(orderBean.status=='PROCESSING'?'撤  单':(orderBean.status=='100'?'正在撤单':'正在撤单')):'撤  单'}")
 	String status;
-	
+
+	@Menu(items={"left","right","search"})
+	IMenu toolbar;
 	
 	@Override
 	public void updateModel(Object value) {
 		if (value instanceof StockTradingOrderBean) {
-			orderBean = (StockTradingOrderBean)value;
-			registerBean("orderBean",value);
+			orderBean = (StockTradingOrderBean) value;
+			registerBean("orderBean", value);
 		}
 	}
-	
-	@Command
+
+	@Command(uiItems=
+			@UIItem(id="right",label="取消",icon="resourceId:drawable/zpb")
+		,navigations = { @Navigation(on = "*", showDialog = "messageBox", params = {
+			@Parameter(name = "title", value = ""),
+			@Parameter(name = "icon", value = "resourceId:drawable/remind_focus"),
+			@Parameter(name = "message", value = "resourceId:message/confirm_cancel_order"),
+			@Parameter(name = "onOK", value = "leftok"),
+			@Parameter(name = "onCanceled", value = "right") }) })
 	String handlerCancelClick(InputEvent event) {
-		//未实现撤单前确认
-		if("100".equals(orderBean.getStatus()))
+		// 未实现撤单前确认
+		// if("100".equals(orderBean.getStatus()))
+		// return null;
+		// manageService.cancelOrder(orderBean.getId()+"");
+		// orderBean.setStatus("100");
+		return "";
+	}
+
+	@Command(uiItems=@UIItem(id="leftok",label="确定",icon="resourceId:drawable/home"))
+	String confirmCancelClick(InputEvent event) {
+		// 未实现撤单前确认
+		if ("100".equals(orderBean.getStatus()))
 			return null;
-		manageService.cancelOrder(orderBean.getId()+"");
+		manageService.cancelOrder(orderBean.getId() + "");
 		orderBean.setStatus("100");
 		return null;
 	}
