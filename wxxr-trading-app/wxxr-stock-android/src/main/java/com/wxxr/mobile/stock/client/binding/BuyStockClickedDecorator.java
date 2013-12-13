e57a6e9caeb4972c15111c8c9c2f7c176480bf35
duzhen <>
2013-12-13 11:26:09 +0000

    FECC-2510 FECC-2511 FECC-2512 FECC-2513

---
+++ wxxr-trading-app/wxxr-stock-android/src/main/java/com/wxxr/mobile/stock/client/binding/BuyStockClickedDecorator.java
@@ -0,0 +1,36 @@
+package com.wxxr.mobile.stock.client.binding;
+
+import com.wxxr.mobile.core.ui.api.IView;
+import com.wxxr.mobile.core.ui.api.InputEvent;
+import com.wxxr.mobile.core.ui.api.InputEventDecorator;
+import com.wxxr.mobile.core.ui.api.InputEventHandlingContext;
+import com.wxxr.mobile.core.ui.common.SimpleInputEvent;
+import com.wxxr.mobile.core.util.IAsyncCallback;
+
+public class BuyStockClickedDecorator implements InputEventDecorator {
+
+	private final InputEventDecorator next;
+	
+	public BuyStockClickedDecorator(InputEventDecorator decor){
+		this.next = decor;
+	}
+	
+	@Override
+	public void handleEvent(InputEventHandlingContext context, InputEvent event) {
+		final IView v = context.getViewModel();
+		IAsyncCallback cb = new IAsyncCallback() {
+			
+			@Override
+			public void success(Object result) {
+				v.hide();
+			}
+			
+			@Override
+			public void failed(Object cause) {
+			}
+		};
+		((SimpleInputEvent)event).addProperty(InputEvent.PROPERTY_CALLBACK, cb);
+		this.next.handleEvent(context, event);
+	}
+
+}
---
+++ wxxr-trading-app/wxxr-stock-android/src/main/java/com/wxxr/mobile/stock/client/binding/StockRefreshClickedDecorator.java
@@ -0,0 +1,44 @@
+package com.wxxr.mobile.stock.client.binding;
+
+import com.wxxr.mobile.core.ui.api.IDataField;
+import com.wxxr.mobile.core.ui.api.IView;
+import com.wxxr.mobile.core.ui.api.InputEvent;
+import com.wxxr.mobile.core.ui.api.InputEventDecorator;
+import com.wxxr.mobile.core.ui.api.InputEventHandlingContext;
+import com.wxxr.mobile.core.ui.common.SimpleInputEvent;
+import com.wxxr.mobile.core.util.IAsyncCallback;
+
+public class StockRefreshClickedDecorator implements InputEventDecorator {
+
+	private final InputEventDecorator next;
+	
+	public StockRefreshClickedDecorator(InputEventDecorator decor){
+		this.next = decor;
+	}
+	
+	@Override
+	public void handleEvent(InputEventHandlingContext context, InputEvent event) {
+		IView v = context.getViewModel();
+		final IDataField<Boolean> fieldProgress = v.getChild("progress",IDataField.class);
+		final IDataField<Boolean> fieldRefresh = v.getChild("refresh",IDataField.class);
+		fieldProgress.setValue(true);
+		fieldRefresh.setValue(false);
+		IAsyncCallback cb = new IAsyncCallback() {
+			
+			@Override
+			public void success(Object result) {
+				fieldProgress.setValue(false);
+				fieldRefresh.setValue(true);
+			}
+			
+			@Override
+			public void failed(Object cause) {
+				fieldProgress.setValue(false);
+				fieldRefresh.setValue(true);
+			}
+		};
+		((SimpleInputEvent)event).addProperty(InputEvent.PROPERTY_CALLBACK, cb);
+		this.next.handleEvent(context, event);
+	}
+
+}
