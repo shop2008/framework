040f81b5b1de8dc7e5563d8c3504efb961101137
renwenjie <>
2013-12-16 10:06:47 +0000

    *** empty log message ***

--- wxxr-trading-app/wxxr-stock-android/src/main/java/com/wxxr/mobile/stock/client/model/InfoNoticeItemView.java
+++ wxxr-trading-app/wxxr-stock-android/src/main/java/com/wxxr/mobile/stock/client/model/InfoNoticeItemView.java
@@ -1,16 +1,25 @@
 package com.wxxr.mobile.stock.client.model;
 
+
+
 import com.wxxr.mobile.android.ui.AndroidBindingType;
 import com.wxxr.mobile.android.ui.annotation.AndroidBinding;
 import com.wxxr.mobile.core.ui.annotation.Bean;
+import com.wxxr.mobile.core.ui.annotation.Command;
 import com.wxxr.mobile.core.ui.annotation.Convertor;
 import com.wxxr.mobile.core.ui.annotation.Field;
+import com.wxxr.mobile.core.ui.annotation.Navigation;
+import com.wxxr.mobile.core.ui.annotation.OnCreate;
+import com.wxxr.mobile.core.ui.annotation.OnShow;
 import com.wxxr.mobile.core.ui.annotation.Parameter;
 import com.wxxr.mobile.core.ui.annotation.View;
+import com.wxxr.mobile.core.ui.annotation.Bean.BindingType;
+import com.wxxr.mobile.core.ui.api.CommandResult;
 import com.wxxr.mobile.core.ui.api.IModelUpdater;
+import com.wxxr.mobile.core.ui.api.InputEvent;
 import com.wxxr.mobile.core.ui.common.ViewBase;
 import com.wxxr.mobile.stock.app.bean.PullMessageBean;
-import com.wxxr.mobile.stock.client.utils.LongTime2StringConvertor;
+import com.wxxr.mobile.stock.app.service.IUserManagementService;
 import com.wxxr.mobile.stock.client.utils.StringTime2StringConvertor;
 
 @View(name="InfoNoticeItemView")
@@ -19,6 +28,9 @@ public abstract class InfoNoticeItemView extends ViewBase implements IModelUpdat
 
 	@Bean
 	PullMessageBean message;
+	
+	@Bean(type = BindingType.Service)
+	IUserManagementService usrService;
 	
 	@Field(valueKey="visible", binding="${message.read==false}")
 	boolean remindReaded;
@@ -32,6 +44,8 @@ public abstract class InfoNoticeItemView extends ViewBase implements IModelUpdat
 	@Field(valueKey="text", binding="${message.message}")
 	String content;
 	
+	
+	
 	@Convertor(
 			params={@Parameter(name="format", value="HH:mm"),
 					@Parameter(name="nullString", value="--:--")
@@ -39,10 +53,24 @@ public abstract class InfoNoticeItemView extends ViewBase implements IModelUpdat
 			)
 	
 	StringTime2StringConvertor stime2StrConvertor;
+	
+	
 	@Override
 	public void updateModel(Object value) {
 		if (value instanceof PullMessageBean) {
 			registerBean("message", value);
+			message = (PullMessageBean) value;
 		}
+	}
+	
+	@Command(navigations = { @Navigation(on = "*", showPage = "webPage") })
+	CommandResult handleNoticesItemClick(InputEvent event) {
+		if (usrService != null) {
+			usrService.readPullMesage(message.getId());
+		}
+		CommandResult result = new CommandResult();
+		result.setPayload(message.getArticleUrl());
+		result.setResult("*");
+		return result;
 	}
 }
