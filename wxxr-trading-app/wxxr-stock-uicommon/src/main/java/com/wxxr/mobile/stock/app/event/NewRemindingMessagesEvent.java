46b364ba04f061a2cc0f4f149d00ced5e364e1c9
neillin <>
2013-12-20 07:48:27 +0000

    refactoring

--- wxxr-trading-app/wxxr-stock-uicommon/src/main/java/com/wxxr/mobile/stock/app/event/NewRemindingMessagesEvent.java
+++ wxxr-trading-app/wxxr-stock-uicommon/src/main/java/com/wxxr/mobile/stock/app/event/NewRemindingMessagesEvent.java
@@ -10,6 +10,7 @@
 package com.wxxr.mobile.stock.app.event;
 
 import com.wxxr.mobile.core.event.api.GenericEventObject;
+import com.wxxr.mobile.core.event.api.IBroadcastEvent;
 import com.wxxr.mobile.stock.app.bean.RemindMessageBean;
 
 /**
@@ -19,7 +20,7 @@ import com.wxxr.mobile.stock.app.bean.RemindMessageBean;
  * @version $Revision$
  * @created time 2013-12-20  下午3:01:30
  */
-public class NewRemindingMessagesEvent extends GenericEventObject {
+public class NewRemindingMessagesEvent extends GenericEventObject implements IBroadcastEvent {
    
    public NewRemindingMessagesEvent(RemindMessageBean[] messages) {     
       super();
