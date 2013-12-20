5361c78c5731ef73c17e7e37dadf0a8b085fad26
renwenjie <>
2013-12-20 07:07:15 +0000

    *** empty log message ***

--- wxxr-trading-app/wxxr-stock-android/src/main/java/com/wxxr/mobile/stock/client/model/ConstructUsPage.java
+++ wxxr-trading-app/wxxr-stock-android/src/main/java/com/wxxr/mobile/stock/client/model/ConstructUsPage.java
@@ -4,17 +4,32 @@ import com.wxxr.mobile.android.ui.AndroidBindingType;
 import com.wxxr.mobile.android.ui.annotation.AndroidBinding;
 import com.wxxr.mobile.core.ui.annotation.Command;
 import com.wxxr.mobile.core.ui.annotation.Field;
+import com.wxxr.mobile.core.ui.annotation.Menu;
+import com.wxxr.mobile.core.ui.annotation.UIItem;
 import com.wxxr.mobile.core.ui.annotation.View;
+import com.wxxr.mobile.core.ui.api.IMenu;
 import com.wxxr.mobile.core.ui.api.InputEvent;
 import com.wxxr.mobile.core.ui.common.PageBase;
 
-@View(name="constructUsPage")
+@View(name="constructUsPage",withToolbar=true, description="联系我们")
 @AndroidBinding(type=AndroidBindingType.FRAGMENT_ACTIVITY, layoutId="R.layout.constract_us_page_layout")
 public abstract class ConstructUsPage extends PageBase {
 
 	@Field(valueKey="text")
 	String version;
 
+	@Menu(items={"left"})
+	private IMenu toolbar;
+	
+	@Command(
+			uiItems={
+				@UIItem(id="left",label="返回",icon="resourceId:drawable/back_button_style")
+			}
+	)
+	String toolbarClickedLeft(InputEvent event){
+		hide();
+		return null;
+	}
 	
 	/**
 	 * 标题栏-"返回"按钮事件处理
