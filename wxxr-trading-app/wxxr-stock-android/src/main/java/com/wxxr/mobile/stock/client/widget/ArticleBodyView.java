15dfffa3f079694e3efb10c2736640bfc74aa01f
renwenjie <>
2013-12-27 07:44:17 +0000

    *** empty log message ***

--- wxxr-trading-app/wxxr-stock-android/src/main/java/com/wxxr/mobile/stock/client/widget/ArticleBodyView.java
+++ wxxr-trading-app/wxxr-stock-android/src/main/java/com/wxxr/mobile/stock/client/widget/ArticleBodyView.java
@@ -15,15 +15,16 @@ import android.webkit.WebView;
 import android.widget.LinearLayout;
 
 public class ArticleBodyView extends ViewGroup {
-	
+
 	private MyWebViewClient webClient;
 	private WebSettings webSettings;
-	private LinearLayout loadingBody; 
+	private LinearLayout loadingBody;
 	private WebView mWebView;
 	private View view;
 	private boolean isLoading = false;
 	private int mArticleViewWidth;
 	private int mArticleViewHeight;
+
 	public ArticleBodyView(Context context, AttributeSet attrs, int defStyle) {
 		super(context, attrs, defStyle);
 		init(context);
@@ -38,22 +39,22 @@ public class ArticleBodyView extends ViewGroup {
 		super(context);
 		init(context);
 	}
-	
+
 	private void init(Context context) {
-		
+
 		LayoutInflater inflater = LayoutInflater.from(context);
-		view = inflater.inflate(R.layout.article_body_layout,this, false);
+		view = inflater.inflate(R.layout.article_body_layout, this, false);
 		loadingBody = (LinearLayout) view.findViewById(R.id.ll_article_body);
 		mWebView = (WebView) view.findViewById(R.id.wv_article_body);
 		webSettings = mWebView.getSettings();
 		webClient = new MyWebViewClient();
 		mWebView.setWebViewClient(webClient);
+		//mWebView.setWebChromeClient(new MyWebViewCromeClient());
 		setWebViewAttribute();
 		isLoading = false;
 		addView(view);
 	}
 
-	
 	@Override
 	protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
 		super.onMeasure(widthMeasureSpec, heightMeasureSpec);
@@ -61,45 +62,44 @@ public class ArticleBodyView extends ViewGroup {
 		mArticleViewWidth = view.getMeasuredWidth();
 		mArticleViewHeight = view.getMeasuredHeight();
 	}
-	
+
 	@SuppressLint("SetJavaScriptEnabled")
 	private void setWebViewAttribute() {
 		webSettings.setJavaScriptEnabled(true);
 		webSettings.setBuiltInZoomControls(true);
-		//webSettings.setJavaScriptCanOpenWindowsAutomatically(true);
+		// webSettings.setJavaScriptCanOpenWindowsAutomatically(true);
 		webSettings.setCacheMode(WebSettings.LOAD_NO_CACHE);
 		mWebView.setInitialScale(0);
 	}
-	
+
 	class MyWebViewClient extends WebViewClient {
-		
+
 		@Override
 		public void onPageStarted(WebView view, String url, Bitmap favicon) {
 			super.onPageStarted(view, url, favicon);
 			isLoading = true;
 			loadingBody.setVisibility(View.VISIBLE);
 		}
-		
+
+		@Override
+		public void onReceivedError(WebView view, int errorCode,
+				String description, String failingUrl) {
+			super.onReceivedError(view, errorCode, description, failingUrl);
+		}
+
 		@Override
 		public void onPageFinished(WebView view, String url) {
 			super.onPageFinished(view, url);
 			isLoading = false;
 			loadingBody.setVisibility(View.GONE);
 		}
-		
-		
+
 	}
-	
-	
+
 	public void loadURL(final String url) {
-		if(isLoading) 
+		if (isLoading)
 			return;
-		new Thread(){
-			
-			public void run() {
-				mWebView.loadUrl(url);
-			};
-		}.start();
+		mWebView.loadUrl(url);
 	}
 
 	@Override
