10a920a109d6055a05271649a4497d29cfe98fcc
wangyan <>
2014-01-02 06:46:08 +0000

    FECC-2651

---
+++ wxxr-trading-app/wxxr-stock-uicommon/src/main/java/com/wxxr/mobile/stock/app/service/IUserLoginManagementService.java
@@ -0,0 +1,32 @@
+package com.wxxr.mobile.stock.app.service;
+
+import com.wxxr.mobile.stock.app.LoginFailedException;
+import com.wxxr.mobile.stock.app.StockAppBizException;
+
+public interface IUserLoginManagementService {
+
+	/**
+	 * 注册用户-使用手机号注册
+	 * 用户使用手机号注册，如果没有注册过，则直接使用短信把密码发给他
+	 * 
+	 * @param phoneNumber - 用户手机号
+	 * @throws StockAppBizException 如果已经注册过，抛出异常-通知客户端已经注册
+	 */
+	void register(String phoneNumber) throws StockAppBizException;
+
+	 
+	/**
+	 * 用户登陆
+	 * @param userId
+	 * @param pwd
+	 * @throws StockAppBizException
+	 */
+	void login(String userId,String pwd) throws LoginFailedException;
+	/**
+	 * 退出登陆
+	 * @param userId
+	 * @param pwd
+	 */
+	void logout();
+
+}
