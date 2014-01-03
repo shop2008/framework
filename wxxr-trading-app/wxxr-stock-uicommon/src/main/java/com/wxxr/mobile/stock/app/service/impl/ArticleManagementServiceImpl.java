83a3a71857e39db31ca69441bcb6d2114d6456dc
xijiadeng <>
2014-01-03 07:08:29 +0000

    FECC-2650

--- wxxr-trading-app/wxxr-stock-uicommon/src/main/java/com/wxxr/mobile/stock/app/service/impl/ArticleManagementServiceImpl.java
+++ wxxr-trading-app/wxxr-stock-uicommon/src/main/java/com/wxxr/mobile/stock/app/service/impl/ArticleManagementServiceImpl.java
@@ -86,7 +86,7 @@ public class ArticleManagementServiceImpl extends AbstractModule<IStockAppContex
 				public int compare(ArticleBean o1, ArticleBean o2) {
 					int flag = 0;
 					flag = o2.getPower() - o1.getPower();
-					if(flag==0){
+					if(flag==0){ 
 						if(StringUtils.isNotBlank(o2.getCreateDate()) || StringUtils.isNotBlank(o1.getCreateDate())){
 							flag = o2.getCreateDate().compareTo(o1.getCreateDate());
 						}
