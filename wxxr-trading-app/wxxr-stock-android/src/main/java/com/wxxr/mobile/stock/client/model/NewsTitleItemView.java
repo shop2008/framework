be2e42ffee93079c41f37877358a1b3de5d35498
xijiadeng <>
2014-01-10 05:59:59 +0000

    *** empty log message ***

--- wxxr-trading-app/wxxr-stock-android/src/main/java/com/wxxr/mobile/stock/client/model/NewsTitleItemView.java
+++ wxxr-trading-app/wxxr-stock-android/src/main/java/com/wxxr/mobile/stock/client/model/NewsTitleItemView.java
@@ -2,6 +2,7 @@ package com.wxxr.mobile.stock.client.model;
 
 import com.wxxr.mobile.android.ui.AndroidBindingType;
 import com.wxxr.mobile.android.ui.annotation.AndroidBinding;
+import com.wxxr.mobile.core.ui.annotation.Bean;
 import com.wxxr.mobile.core.ui.annotation.Field;
 import com.wxxr.mobile.core.ui.annotation.View;
 import com.wxxr.mobile.core.ui.api.IModelUpdater;
@@ -11,14 +12,17 @@ import com.wxxr.mobile.core.ui.common.ViewBase;
 @AndroidBinding(type=AndroidBindingType.VIEW, layoutId="R.layout.news_title_layout")
 public abstract class NewsTitleItemView extends ViewBase implements IModelUpdater {
 
+	@Bean
+	String titleValue;
 	
-	@Field(valueKey="text", binding="${title!=null?title:''}")
+	@Field(valueKey="text", binding="${titleValue!=null?titleValue:null}",visibleWhen="${titleValue!=null}")
 	String title;
 	
 	@Override
 	public void updateModel(Object value) {
 		if (value instanceof String) {
-			registerBean("title", value);
+			this.titleValue = (String) value;
+			registerBean("titleValue", this.titleValue);
 		}
 	}
 }
