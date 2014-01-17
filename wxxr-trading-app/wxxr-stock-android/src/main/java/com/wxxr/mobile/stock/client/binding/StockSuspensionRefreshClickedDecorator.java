7f9f60f662b1d9a11e36f7494d64ac4daa7cbbfb
duzhen <>
2014-01-17 05:29:29 +0000

    FECC-2690

--- wxxr-trading-app/wxxr-stock-android/src/main/java/com/wxxr/mobile/stock/client/binding/StockSuspensionRefreshClickedDecorator.java
+++ wxxr-trading-app/wxxr-stock-android/src/main/java/com/wxxr/mobile/stock/client/binding/StockSuspensionRefreshClickedDecorator.java
@@ -1,5 +1,8 @@
 package com.wxxr.mobile.stock.client.binding;
 
+import java.util.concurrent.TimeUnit;
+
+import com.wxxr.mobile.android.app.AppUtils;
 import com.wxxr.mobile.core.ui.api.IDataField;
 import com.wxxr.mobile.core.ui.api.IView;
 import com.wxxr.mobile.core.ui.api.InputEvent;
@@ -11,38 +14,50 @@ import com.wxxr.mobile.core.util.IAsyncCallback;
 public class StockSuspensionRefreshClickedDecorator implements InputEventDecorator {
 
 	private final InputEventDecorator next;
-
+	private long handleTime = 0;
+	
 	public StockSuspensionRefreshClickedDecorator(InputEventDecorator decor) {
 		this.next = decor;
 	}
 
 	@Override
 	public void handleEvent(InputEventHandlingContext context, InputEvent event) {
-		IView v = context.getViewModel();
-		final IDataField<Boolean> fieldProgress = v.getChild("progressSus",
-				IDataField.class);
-		final IDataField<Boolean> fieldRefresh = v.getChild("refreshSus",
-				IDataField.class);
-		if (fieldProgress != null)
-			fieldProgress.setValue(true);
-		if (fieldRefresh != null)
-			fieldRefresh.setValue(false);
+		final IView v = context.getViewModel();
+		setProgressFieldStatus(v, true);
+		handleTime = System.currentTimeMillis();
+
 		IAsyncCallback cb = new IAsyncCallback() {
 
 			@Override
 			public void success(Object result) {
-				if (fieldProgress != null)
-					fieldProgress.setValue(false);
-				if (fieldRefresh != null)
-					fieldRefresh.setValue(true);
+				long time = System.currentTimeMillis();
+				if (time - handleTime < 1000) {
+					AppUtils.invokeLater(new Runnable() {
+
+						@Override
+						public void run() {
+							setProgressFieldStatus(v, false);
+						}
+					}, 500, TimeUnit.MILLISECONDS);
+				} else {
+					setProgressFieldStatus(v, false);
+				}
 			}
 
 			@Override
 			public void failed(Object cause) {
-				if (fieldProgress != null)
-					fieldProgress.setValue(false);
-				if (fieldRefresh != null)
-					fieldRefresh.setValue(true);
+				long time = System.currentTimeMillis();
+				if (time - handleTime < 1000) {
+					AppUtils.invokeLater(new Runnable() {
+
+						@Override
+						public void run() {
+							setProgressFieldStatus(v, false);
+						}
+					}, 500, TimeUnit.MILLISECONDS);
+				} else {
+					setProgressFieldStatus(v, false);
+				}
 			}
 		};
 		((SimpleInputEvent) event)
@@ -50,4 +65,14 @@ public class StockSuspensionRefreshClickedDecorator implements InputEventDecorat
 		this.next.handleEvent(context, event);
 	}
 
+	private void setProgressFieldStatus(IView v, boolean status) {
+		IDataField<Boolean> fieldProgress = v.getChild("progressSus",
+				IDataField.class);
+		IDataField<Boolean> fieldRefresh = v.getChild("refreshSus",
+				IDataField.class);
+		if (fieldProgress != null)
+			fieldProgress.setValue(status);
+		if (fieldRefresh != null)
+			fieldRefresh.setValue(!status);
+	}
 }
