a4f1494988353cc90ac55eeef765daec939e765c
renwenjie <>
2014-01-17 03:39:28 +0000

    *** empty log message ***

--- wxxr-trading-app/wxxr-stock-android/src/main/java/com/wxxr/mobile/stock/client/model/InfoNoticeTitleView.java
+++ wxxr-trading-app/wxxr-stock-android/src/main/java/com/wxxr/mobile/stock/client/model/InfoNoticeTitleView.java
@@ -2,6 +2,7 @@ package com.wxxr.mobile.stock.client.model;
 
 import com.wxxr.mobile.android.ui.AndroidBindingType;
 import com.wxxr.mobile.android.ui.annotation.AndroidBinding;
+import com.wxxr.mobile.core.ui.annotation.Bean;
 import com.wxxr.mobile.core.ui.annotation.Field;
 import com.wxxr.mobile.core.ui.annotation.View;
 import com.wxxr.mobile.core.ui.api.IModelUpdater;
@@ -11,14 +12,18 @@ import com.wxxr.mobile.core.ui.common.ViewBase;
 @AndroidBinding(type=AndroidBindingType.VIEW, layoutId="R.layout.info_notice_title_layout")
 public abstract class InfoNoticeTitleView extends ViewBase implements IModelUpdater {
 
-	@Field(valueKey="text", binding="${title}")
+	@Field(valueKey="text", binding="${titleValue!=null?titleValue:null}", visibleWhen="${titleValue!=null}")
 	String title;
 	
+	@Bean
+	String titleValue;
 	@Override
 	public void updateModel(Object value) {
 			
 		if (value instanceof String) {
-			registerBean("title", value);
+			
+			this.titleValue = (String) value;
+			registerBean("titleValue", this.titleValue);
 		}
 	}	
 }
