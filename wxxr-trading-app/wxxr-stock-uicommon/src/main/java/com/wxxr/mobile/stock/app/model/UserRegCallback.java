1be95b4fc276d985cf42c3c566ea2cc599be2448
renwenjie <>
2014-02-14 06:26:38 +0000

    *** empty log message ***

--- wxxr-trading-app/wxxr-stock-uicommon/src/main/java/com/wxxr/mobile/stock/app/model/UserRegCallback.java
+++ wxxr-trading-app/wxxr-stock-uicommon/src/main/java/com/wxxr/mobile/stock/app/model/UserRegCallback.java
@@ -4,11 +4,32 @@ public class UserRegCallback {
 	
 	private String userName;
 
+	private String password;
+	
+	private String retypePassword;
+	
+	
 	public String getUserName() {
 		return userName;
 	}
 
 	public void setUserName(String userName) {
 		this.userName = userName;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getRetypePassword() {
+		return retypePassword;
+	}
+
+	public void setRetypePassword(String retypePassword) {
+		this.retypePassword = retypePassword;
 	}
 }
